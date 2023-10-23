69fb230eebfa548a6fce51a9706a1e3873cff34b
함소명 <gkathaud@naver.com>
2023-10-23 14:36:13 +0900

    feat: application.yml 추가

--- backend/HereHear/src/main/java/com/ssafy/herehear/HereHearApplication.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/HereHearApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.herehear;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HereHearApplication {
 
     public static void main(String[] args) {
