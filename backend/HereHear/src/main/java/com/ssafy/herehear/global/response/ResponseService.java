98e1c5294c787408022b4a7fe8d78fe51e2db920
김주하 <tkfkd122@naver.com>
2023-10-24 14:02:00 +0900

    modify: 단순 코드 수정

--- backend/HereHear/src/main/java/com/ssafy/herehear/global/response/ResponseService.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/global/response/ResponseService.java
@@ -19,6 +19,4 @@ public class ResponseService {
         dataResponse.setData(data);
         return dataResponse;
     }
-
-    public CommonResponse failureCommmonResponse(ResponseStatus responseStatus)
 }
