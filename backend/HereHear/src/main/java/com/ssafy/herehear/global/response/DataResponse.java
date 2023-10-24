85cda8a7e04102bb5f435f012e5f2ea1e5a1d84d
김주하 <tkfkd122@naver.com>
2023-10-24 13:43:06 +0900

    feat: custom response 추가

---
+++ backend/HereHear/src/main/java/com/ssafy/herehear/global/response/DataResponse.java
@@ -0,0 +1,15 @@
+package com.ssafy.herehear.global.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class DataResponse<T> extends  CommonResponse {
+
+    private T data;
+
+
+}
