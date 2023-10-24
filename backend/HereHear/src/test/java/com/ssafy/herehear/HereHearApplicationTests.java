8b5e50986d202932002217937d8cb3ff21391eef
김주하 <tkfkd122@naver.com>
2023-10-24 12:19:24 +0900

    modify: testcode 주석처리

--- backend/HereHear/src/test/java/com/ssafy/herehear/HereHearApplicationTests.java
+++ backend/HereHear/src/test/java/com/ssafy/herehear/HereHearApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class HereHearApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
