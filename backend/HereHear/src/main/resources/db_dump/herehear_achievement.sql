2bb191361795826f7fa6e79fb7fd48920f9e0dff
ChanHwan <song960705@naver.com>
2023-10-25 20:47:07 +0900

    feat: 업적등록 관리자API 구현

---
+++ backend/HereHear/src/main/resources/db_dump/herehear_achievement.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: k9b202.p.ssafy.io    Database: herehear
+-- ------------------------------------------------------
+-- Server version	8.1.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `achievement`
+--
+
+DROP TABLE IF EXISTS `achievement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `achievement` (
+  `achievement_id` bigint NOT NULL AUTO_INCREMENT,
+  `border_code` bigint DEFAULT NULL,
+  `title_code` bigint DEFAULT NULL,
+  `icon` varchar(255) DEFAULT NULL,
+  `mission` text,
+  PRIMARY KEY (`achievement_id`),
+  UNIQUE KEY `UK_8lg1fxby7qd2qx42doswwojuc` (`border_code`),
+  UNIQUE KEY `UK_a4rqla4g9erquq57ipom3vww9` (`title_code`),
+  CONSTRAINT `FK30qlvyojsgtab6qp7hbub44ma` FOREIGN KEY (`title_code`) REFERENCES `title_code` (`title_code`),
+  CONSTRAINT `FKf1q4gi2qd15qixhjeevy1tbxn` FOREIGN KEY (`border_code`) REFERENCES `border_code` (`border_code`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `achievement`
+--
+
+LOCK TABLES `achievement` WRITE;
+/*!40000 ALTER TABLE `achievement` DISABLE KEYS */;
+INSERT INTO `achievement` VALUES (1,1,1,'','사용자가 최초로 음악을 등록 했을 때'),(2,2,2,'','사용자가 10곡을 등록했을 때'),(3,3,3,'','사용자가 50곡을 등록했을 때'),(4,4,4,'','사용자가 100곡 등록했을 때'),(5,5,5,'','사용자가 200곡 등록했을 때'),(6,6,6,'','사용자가 등록한 음악이 좋아요를 10개 받았을 때'),(7,7,7,'','사용자가 등록한 음악이 좋아요를 500개 받았을 때'),(8,8,8,'','내가 등록한 음악의 누적합 최초 100개 좋아요를 받았을 때'),(9,9,9,'','팔로워가 10명 돌파 했을 때'),(10,10,10,'','팔로워가 50명 돌파 했을 때'),(11,11,11,'','팔로워가 100명 돌파 했을 때'),(12,12,13,'','팔로워가 500명 돌파 했을 때');
+/*!40000 ALTER TABLE `achievement` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-25 20:42:53
