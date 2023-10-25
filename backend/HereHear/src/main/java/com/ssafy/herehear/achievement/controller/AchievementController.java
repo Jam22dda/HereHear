bf6dc767f4c60d13a18e8688d3227e5706e98239
ChanHwan <song960705@naver.com>
2023-10-26 07:37:17 +0900

    feat: 내 테두리 목록 조회 API 구현

--- backend/HereHear/src/main/java/com/ssafy/herehear/achievement/controller/AchievementController.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/achievement/controller/AchievementController.java
@@ -1,6 +1,7 @@
 package com.ssafy.herehear.achievement.controller;
 
 import com.ssafy.herehear.achievement.dto.AchievementDto;
+import com.ssafy.herehear.achievement.dto.BorderCodeDto;
 import com.ssafy.herehear.achievement.dto.MemberAchievementDto;
 import com.ssafy.herehear.achievement.dto.TitleCodeDto;
 import com.ssafy.herehear.achievement.service.AchievementService;
@@ -25,7 +26,7 @@ public class AchievementController {
 
     @GetMapping("/list")
     public DataResponse<List<AchievementDto>> getAchievementList() {
-        log.info("[업적리스트 조회] {}", TimeFormatUtil.formatTime(LocalDateTime.now()));
+        log.info("[업적리스트 조회] time: {}", TimeFormatUtil.formatTime(LocalDateTime.now()));
 
         List<AchievementDto> achievementDtoList = achievementService.getAchievementList();
         return new DataResponse<>("200", "업적리스트 조회", achievementDtoList);
@@ -36,7 +37,7 @@ public class AchievementController {
         // TODO: OAuth2.0으로 받아온 userId로 변경해야 함
         Long memberId = 1L;
 
-        log.info("[달성한 업적 조회] memberId - {}, time - {}", memberId, TimeFormatUtil.formatTime(LocalDateTime.now()));
+        log.info("[달성한 업적 조회] memberId: {}, time: {}", memberId, TimeFormatUtil.formatTime(LocalDateTime.now()));
 
         List<MemberAchievementDto> myAchievementList = achievementService.getMyAchievementList(memberId);
 
@@ -48,15 +49,31 @@ public class AchievementController {
         // TODO: OAuth2.0으로 받아온 userId로 변경해야 함
         Long memberId = 1L;
 
-        log.info("[내 칭호 목록 조회] memberId - {}, time - {}", memberId, TimeFormatUtil.formatTime(LocalDateTime.now()));
+        log.info("[내 칭호 목록 조회] memberId: {}, time: {}", memberId, TimeFormatUtil.formatTime(LocalDateTime.now()));
 
-        List<MemberAchievementDto> myAchievementList = achievementService.getMyAchievementList(1L);
+        List<MemberAchievementDto> myAchievementList = achievementService.getMyAchievementList(memberId);
 
         List<TitleCodeDto> myTitleList = myAchievementList.stream()
                 .map(MemberAchievementDto::getTitle)
                 .toList();
 
         return new DataResponse<>("200", "내 칭호 목록 조회", myTitleList);
+    }
+
+    @GetMapping("/myborder")
+    public DataResponse<List<BorderCodeDto>> getMyBorder() {
+        // TODO: OAuth2.0으로 받아온 userId로 변경해야 함
+        Long memberId = 1L;
+
+        log.info("[내 테두리 목록 조회] memberId: {}, time: {}", memberId, TimeFormatUtil.formatTime(LocalDateTime.now()));
+
+        List<MemberAchievementDto> myAchievementList = achievementService.getMyAchievementList(memberId);
+
+        List<BorderCodeDto> myBorderList = myAchievementList.stream()
+                .map(MemberAchievementDto::getBorder)
+                .toList();
+
+        return new DataResponse<>("200", "내 테두리 목록 조회", myBorderList);
     }
 
 }
