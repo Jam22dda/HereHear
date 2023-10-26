c939b2410351e7f5c19e8a761171d94971e2b03e
함소명 <gkathaud@naver.com>
2023-10-26 17:56:48 +0900

    modify: RegisteredMusic Repository 수정

--- backend/HereHear/src/main/java/com/ssafy/herehear/music/repository/RegisteredMusicRepositoryImpl.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/music/repository/RegisteredMusicRepositoryImpl.java
@@ -3,10 +3,12 @@ package com.ssafy.herehear.music.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.ssafy.herehear.entity.RegisteredMusic;
-import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+
+import static com.ssafy.herehear.entity.QMusic.music;
+import static com.ssafy.herehear.entity.QRegisteredMusic.registeredMusic;
 
 @RequiredArgsConstructor
 public class RegisteredMusicRepositoryImpl implements RegisteredMusicRepositoryCustom {
@@ -15,6 +17,9 @@ public class RegisteredMusicRepositoryImpl implements RegisteredMusicRepositoryC
 
     @Override
     public List<RegisteredMusic> findByMyRegisterMusics(long memberId) {
-        return Collections.emptyList();
+        return jpaQueryFactory.selectFrom(registeredMusic)
+                .innerJoin(registeredMusic.music, music)
+                .where(registeredMusic.music.musicId.eq(memberId))
+                .fetch();
     }
 }
