83aad7dee6f2b21026e6bc38711d52fd71b20905
samsung <gkathaud@naver.com>
2023-10-27 12:36:18 +0900

    modify: QueryDSL 및 mapper 저장 변경

--- backend/HereHear/src/main/java/com/ssafy/herehear/music/repository/RegisteredMusicRepositoryCustom.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/music/repository/RegisteredMusicRepositoryCustom.java
@@ -3,9 +3,16 @@ package com.ssafy.herehear.music.repository;
 import com.ssafy.herehear.entity.RegisteredMusic;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RegisteredMusicRepositoryCustom {
 
+    List<RegisteredMusic> findByRegisterMusics();
+
+    Optional<RegisteredMusic> findByRegisterMusic(long registeredMusicId);
+
     List<RegisteredMusic> findByMyRegisterMusics(long memberId);
+
+    Optional<RegisteredMusic> findByMyRegisterMusic(long memberId, long registeredMusicId);
 
 }
