bfac3ed771e6986e54c8e5c4b2427ba1487e6257
seol <seolhyehyeon@gmail.com>
2023-10-27 11:17:09 +0900

    fix : 버튼 안나오는 문제 해결

--- frontend/HereHear/src/main.tsx
+++ frontend/HereHear/src/main.tsx
@@ -1,48 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Landing from './Pages/Landing.tsx';
-import Achievement from './Pages/Achievement.tsx';
-import Core from './Pages/Core.tsx';
-import Follow from './Pages/Follow.tsx';
-import Following from './Pages/Following.tsx';
-import Like from './Pages/Like.tsx';
-import ListenedMusic from './Pages/ListenedMusic.tsx';
-import Member from './Pages/Member.tsx';
-import MyRegist from './Pages/MyRegist.tsx';
-import RegistMusic from './Pages/RegistMusic.tsx';
-import SelectMusic from './Pages/SelectMusic.tsx';
-import Stats from './Pages/Stats.tsx';
-import App from './App.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./Pages/Landing.tsx";
+import Achievement from "./Pages/Achievement.tsx";
+import Core from "./Pages/Core.tsx";
+import Follow from "./Pages/Follow.tsx";
+import Following from "./Pages/Following.tsx";
+import Like from "./Pages/Like.tsx";
+import ListenedMusic from "./Pages/ListenedMusic.tsx";
+import Member from "./Pages/Member.tsx";
+import MyRegist from "./Pages/MyRegist.tsx";
+import RegistMusic from "./Pages/RegistMusic.tsx";
+import SelectMusic from "./Pages/SelectMusic.tsx";
+import Stats from "./Pages/Stats.tsx";
+import App from "./App.tsx";
+import theme from "./styles/theme.ts";
+// import "@/styles/font.css";
 
 // declare global {
 //     interface Window {
 //         naver: any;
 //     }
 // }
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Landing />}></Route>
+            {/* <RecoilRoot> */}
+            <ThemeProvider theme={theme}>
+                <Routes>
+                    <Route path="/" element={<Landing />}></Route>
 
-                <Route path='/core' element={<Core />}></Route>
-                <Route path='/achievement' element={<Achievement />}></Route>
-                <Route path='/registMusic' element={<RegistMusic />}></Route>
-                <Route path='/selectMusic' element={<SelectMusic />}></Route>
-                <Route path='/stats' element={<Stats />}></Route>
+                    <Route path="/core" element={<Core />}></Route>
+                    <Route path="/achievement" element={<Achievement />}></Route>
+                    <Route path="/registMusic" element={<RegistMusic />}></Route>
+                    <Route path="/selectMusic" element={<SelectMusic />}></Route>
+                    <Route path="/stats" element={<Stats />}></Route>
 
-                <Route path='/follow' element={<Follow />}></Route>
-                <Route path='/following' element={<Following />}></Route>
-                <Route path='/like' element={<Like />}></Route>
-                <Route path='/listenedMusic' element={<ListenedMusic />}></Route>
-                <Route path='/member/:id' element={<Member />}></Route>
-                <Route path='/myRegist' element={<MyRegist />}></Route>
+                    <Route path="/follow" element={<Follow />}></Route>
+                    <Route path="/following" element={<Following />}></Route>
+                    <Route path="/like" element={<Like />}></Route>
+                    <Route path="/listenedMusic" element={<ListenedMusic />}></Route>
+                    <Route path="/member/:id" element={<Member />}></Route>
+                    <Route path="/myRegist" element={<MyRegist />}></Route>
 
-                {/* 출시 시 제거해야 함, 테스트용 */}
-                <Route path='/app' element={<App />}></Route>
-            </Routes>
+                    {/* 출시 시 제거해야 함, 테스트용 */}
+                    <Route path="/app" element={<App />}></Route>
+                </Routes>
+            </ThemeProvider>
+            {/* </RecoilRoot> */}
         </BrowserRouter>
     </React.StrictMode>
 );
