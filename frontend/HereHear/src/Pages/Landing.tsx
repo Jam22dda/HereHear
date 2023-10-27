ee2218eecb5a1836a956ce8178969b4a0149bc97
seol <seolhyehyeon@gmail.com>
2023-10-27 15:54:26 +0900

    fix : circleBtn 수정

--- frontend/HereHear/src/Pages/Landing.tsx
+++ frontend/HereHear/src/Pages/Landing.tsx
@@ -1,6 +1,7 @@
 // import { Link, useNavigate } from "react-router-dom";
 import React from "react";
 import Button from "../components/atoms/Button/Button";
+import CircleButton from "../components/atoms/CircleButton/CircleButton";
 
 export default function Landing() {
     // const id = "angel";
@@ -31,6 +32,7 @@ export default function Landing() {
                 <br />
                 <button onClick={moveAppHandler}>Go to App</button> */}
                 <Button>버튼</Button>
+                <CircleButton>원</CircleButton>
                 {/* </div>
             <div className="container">
                 <h1>제목 : 700</h1>
--- frontend/HereHear/src/components/atoms/CircleButton/CircleButton.styles.ts
+++ frontend/HereHear/src/components/atoms/CircleButton/CircleButton.styles.ts
@@ -1,100 +1,79 @@
 import styled, { css } from "styled-components";
 import { CircleButtonProps } from "./CircleButton";
-// import theme from "@/styles/theme";
+import theme from "../../../styles/theme";
 
-const getOptionStyling = (
-  option: Required<CircleButtonProps>["option"],
-  props: CircleButtonProps
-) => {
-  const style = {
-    default: css`
-      background: ${({ theme }) => theme.color.white3};
-      color: ${({ theme }) => theme.color.blue};
-      border: none;
-    `,
-    pinkActivated: css`
-      background: ${({ theme }) => theme.color.blue};
-      color: ${({ theme }) => theme.color.white};
-      border: none;
-    `,
-    pinkDeActivated: css`
-      background: ${({ theme }) => theme.color.grey5};
-      border: none;
-      color: ${({ theme }) => theme.color.grey2};
-    `,
-    GradActivated: css`
-      background: ${({ theme }) => theme.color.white};
-      border: none;
-      color: ${({ theme }) => theme.color.grey1};
-    `,
-    GradDeActivated: css`
-      background: ${({ theme }) => theme.color.danger};
-      border: none;
-      color: ${({ theme }) => theme.color.white};
-    `,
-    //TODO: 활성화 이걸로 사용해보기..
-    keypad: css`
-      background: ${({ theme }) => theme.color.grey5};
-      color: ${({ theme }) => theme.color.grey2};
-      font-weight: bold;
-      font-size: ${({ theme }) => theme.fontSize.body1};
-
-      &:active {
-        background: ${({ theme }) => theme.color.blue};
-        color: ${({ theme }) => theme.color.white};
-
-        .invert {
-          filter: invert(1);
-        }
-      }
-    `,
-  };
-  return props.$backgroundColor
-    ? css`
-        background: ${props.$backgroundColor};
-      `
-    // : style[option];
+const getOptionStyling = (option: Required<CircleButtonProps>["option"], props: CircleButtonProps) => {
+    const style = {
+        default: css`
+            background: ${({ theme }) => theme.color.white2};
+            color: ${({ theme }) => theme.color.main1};
+            box-shadow: ${({ theme }) => theme.shadow.shadow_play1};
+        `,
+        pinkActivated: css`
+            background: ${({ theme }) => theme.color.pink1};
+            color: ${({ theme }) => theme.color.pink2};
+            box-shadow: ${({ theme }) => theme.shadow.shadow_smallbtn};
+        `,
+        pinkDeActivated: css`
+            background: ${({ theme }) => theme.color.pink1};
+            color: ${({ theme }) => theme.color.pink2};
+            box-shadow: ${({ theme }) => theme.shadow.shadow_btn};
+        `,
+        gradActivated: css`
+            background: ${({ theme }) => theme.gradient.gradient1};
+            color: ${({ theme }) => theme.color.pink2};
+            box-shadow: ${({ theme }) => theme.shadow.shadow_heart};
+        `,
+        gradDeActivated: css`
+            background: ${({ theme }) => theme.gradient.gradient1};
+            color: ${({ theme }) => theme.color.pink2};
+            box-shadow: ${({ theme }) => theme.shadow.shadow_play1};
+            font-weight: bold;
+        `,
+    };
+    return props.$backgroundColor
+        ? css`
+              background: ${props.$backgroundColor};
+          `
+        : style[option];
 };
 
 const getSizeStyling = (size: Required<CircleButtonProps>["size"]) => {
-  const style = {
-    small: css`
-      height: 40px;
-      width: 40px;
-      border-radius: 15px;
-    `,
-    medium1: css`
-      height: 40px;
-      width: 40px;
-      border-radius: 20px;
-    `,
-    medium2: css`
-      height: 45px;
-      width: 45px;
-      border-radius: 22.5px;
-    `,
-    large: css`
-      height: 50px;
-      width: 50px;
-      border-radius: 25px;
-    `,
-  };
-  return style[size];
+    const style = {
+        small: css`
+            height: 40px;
+            width: 40px;
+            border-radius: 15px;
+        `,
+        medium: css`
+            height: 40px;
+            width: 40px;
+            border-radius: 20px;
+        `,
+        mediumplus: css`
+            height: 45px;
+            width: 45px;
+            border-radius: 22.5px;
+        `,
+        large: css`
+            height: 50px;
+            width: 50px;
+            border-radius: 25px;
+        `,
+    };
+    return style[size];
 };
 
 const Button = styled.button<CircleButtonProps>`
-  width: ${(props) => props.$width || "100%"};
-  text-align: ${(props) => props.$textAlign || "center"};
-  color: ${(props) =>
-    props.color ? props.theme.color[props.color] : theme.color.black1};
-  border: ${(props) =>
-    props.$border ||
-    `1.5px solid ${props.$borderColor || props.theme.color.blue}`};
-  border-radius: ${(props) => props.$borderRadius || "23px"};
-  ${({ size = "large" }) => getSizeStyling(size)};
-  ${({ option = "default", ...props }) => getOptionStyling(option, props)};
-  font-size: ${(props) => props.$fontSize || "20px"};
-  background-color: ${(props) => props.$backgroundColor};
+    width: ${(props) => props.$width || "100%"};
+    text-align: ${(props) => props.$textAlign || "center"};
+    color: ${(props) => (props.color ? props.theme.color[props.color] : theme.color.main1)};
+    border-radius: ${(props) => props.$borderRadius || "24px"};
+    ${({ size = "large" }) => getSizeStyling(size)};
+    ${({ option = "default", ...props }) => getOptionStyling(option, props)};
+    font-size: ${(props) => props.$fontSize || "20px"};
+    background-color: ${(props) => props.$backgroundColor};
+    box-shadow: ${(props) => props.$shadow};
 `;
 
 export { Button };
