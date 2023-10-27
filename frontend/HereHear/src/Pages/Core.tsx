391168a72f4fb9fe426d12037152722b014c8690
AngelPlayer <ssh5212@naver.com>
2023-10-27 09:06:36 +0900

    feat: 지도 화면 출력

--- frontend/HereHear/src/Pages/Core.tsx
+++ frontend/HereHear/src/Pages/Core.tsx
@@ -1,9 +1,36 @@
+import { useEffect } from 'react';
+import './Core.css';
+
 export default function Core() {
+    useEffect(() => {
+        const apiKey = import.meta.env.VITE_NAVER_MAP_API_KEY;
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${apiKey}`;
+
+        script.onload = () => {
+            // 전역 변수 naver 사용
+            const naver = window.naver;
+
+            const mapOptions = {
+                center: new naver.maps.LatLng(37.3595704, 127.105399),
+                zoom: 10,
+            };
+
+            const map = new naver.maps.Map('map', mapOptions);
+        };
+
+        document.body.appendChild(script);
+
+        // 컴포넌트 언마운트 시 스크립트 제거
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
     return (
-        <div id='display'>
-            <div className='container'>
-                <h1>Core</h1>
-            </div>
+        <div id='map__display'>
+            <div id='map'></div>
         </div>
     );
 }
