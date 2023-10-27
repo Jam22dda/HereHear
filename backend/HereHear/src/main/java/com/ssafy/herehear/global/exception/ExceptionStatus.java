ed9b85cd271d2d4887f757bd932b68cd4492e8f8
김주하 <tkfkd122@naver.com>
2023-10-28 00:36:29 +0900

    feat: member exception status 추가

--- backend/HereHear/src/main/java/com/ssafy/herehear/global/exception/ExceptionStatus.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/global/exception/ExceptionStatus.java
@@ -12,7 +12,9 @@ public enum ExceptionStatus {
 
     TOKEN_EXPIRED("-1000", "토큰이 만료되었습니다."),
     REFRESH_TOKEN_EXPIRED("-1001", "토큰이 만료되었습니다."),
-    TOKEN_NOT_FOUND_IN_COOKIE("-1002", "토큰이 없습니다.");
+    TOKEN_NOT_FOUND_IN_COOKIE("-1002", "토큰이 없습니다."),
+
+    MEMBER_NOT_FOUND("-2000", "멤버를 찾을 수 없습니다.");
 
     private final String code;
     private final String message;
