2251c9d3e827dd9817683ebf93cd0c1298046ddc
seolhyehyeon <seolhyehyeon@gmail.com>
2023-10-30 04:58:48 +0900

    feat:음악재생,메세지,메세지등록,팔로우,음악핀 생성

--- frontend/HereHear/src/Pages/Core.tsx
+++ frontend/HereHear/src/Pages/Core.tsx
@@ -1,57 +1,63 @@
-import { useEffect, useState } from 'react';
-import './Core.css';
-import testImage from '../assets/Core/test.png';
+import { useEffect, useState } from "react";
+import { Map, MapDisplay } from "./Core.styles"; // 스타일을 가져옴
+import testImage from "../assets/Core/test.png";
+
+// import { Image } from "../components/atoms/Image/Image";
+// import MusicBin from "../../../assets/Core/AlbumArtPin.png"
 
 export default function Core() {
-    const [isUpdate, setIsUpdate] = useState(false);
+  const [isUpdate, setIsUpdate] = useState(false);
 
-    useEffect(() => {
-        const apiKey = import.meta.env.VITE_NAVER_MAP_API_KEY;
-        const script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${apiKey}`;
+  useEffect(() => {
+    const apiKey = import.meta.env.VITE_NAVER_MAP_API_KEY;
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${apiKey}`;
 
-        script.onload = () => {
-            // 전역 변수 naver 사용
-            const naver = window.naver;
+    script.onload = () => {
+      // 전역 변수 naver 사용
+      const naver = window.naver;
 
-            const mapOptions = {
-                center: new naver.maps.LatLng(37.3595704, 127.105399),
-                zoom: 10,
-            };
+      const mapOptions = {
+        center: new naver.maps.LatLng(37.3595704, 127.105399),
+        zoom: 10,
+      };
 
-            const map = new naver.maps.Map('map', mapOptions);
+      const map = new naver.maps.Map("map", mapOptions);
 
-            const arr = [];
-            let w = 37.4867995957995;
-            for (let i = 0; i < 10000; i++) {
-                arr[i] = new naver.maps.Marker({
-                    position: new naver.maps.LatLng(37.4867995957995 + i * 0.1, 126.982211871752),
-                    map: map,
-                    icon: {
-                        content: `
+      const arr = [];
+      const w = 37.4867995957995;
+      for (let i = 0; i < 10000; i++) {
+        arr[i] = new naver.maps.Marker({
+          position: new naver.maps.LatLng(
+            37.4867995957995 + i * 0.1,
+            126.982211871752
+          ),
+          map: map,
+          icon: {
+            content: `
                         
                         <img alt='img' src='${testImage}' />
                         `,
-                    },
-                });
-            }
-        };
+          },
+        });
+      }
+    };
 
-        document.body.appendChild(script);
+    document.body.appendChild(script);
 
-        // 컴포넌트 언마운트 시 스크립트 제거
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, [isUpdate]);
+    // 컴포넌트 언마운트 시 스크립트 제거
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [isUpdate]);
 
-    return (
-        <div id='map__display'>
-            <div id='map'></div>
-        </div>
-        // <div>
-        //     <img alt={`${testImage}`} src={`${testImage}`} />
-        // </div>
-    );
+  return (
+    <MapDisplay>
+      <Map></Map>
+    </MapDisplay>
+    // <div>
+    //     <img alt={`${testImage}`} src={`${testImage}`} />
+    // </div>
+  );
 }
