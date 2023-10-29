a72ea7cf76125d2c4487c123d5bb26db0c55c108
samsung <gkathaud@naver.com>
2023-10-30 01:03:44 +0900

    feat: 좋아요 등록 api 추가

---
+++ backend/HereHear/src/main/java/com/ssafy/herehear/like/controller/LikeMusicController.java
@@ -0,0 +1,37 @@
+package com.ssafy.herehear.like.controller;
+
+import com.ssafy.herehear.global.response.CommonResponse;
+import com.ssafy.herehear.global.response.DataResponse;
+import com.ssafy.herehear.like.service.LikeMusicService;
+import com.ssafy.herehear.music.dto.request.MusicRegisteredIdReqDto;
+import com.ssafy.herehear.music.dto.response.LikeRegisteredMusicResDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/like")
+@RequiredArgsConstructor
+public class LikeMusicController {
+
+    private final LikeMusicService likeMusicService;
+
+    @PostMapping
+    public CommonResponse registerlikeMusic(@RequestHeader("Member-id") Long memberId, @RequestBody MusicRegisteredIdReqDto req){
+        likeMusicService.registerlikeMusic(memberId,req.getRegisteredMusicId());
+        return new CommonResponse("200","좋아요 등록");
+    }
+
+//    @DeleteMapping
+//    public CommonResponse deletelikeMusic(@RequestHeader("Member-id") Long memberId, @RequestBody MusicRegisteredIdReqDto req){
+//        likeMusicService.deletelikeMusic(memberId, req.getRegisteredMusicId());
+//        return new CommonResponse("200", "좋아요 취소");
+//    }
+//
+//    @GetMapping("/list")
+//    public DataResponse<List<LikeRegisteredMusicResDto>> likeMusicList(@RequestHeader("Member-id") Long memberId) {
+//        return new DataResponse<>("200", "좋아요 음악 목록 조회", likeMusicService.likeMusicList(memberId));
+//    }
+
+}
---
+++ backend/HereHear/src/main/java/com/ssafy/herehear/like/service/LikeMusicService.java
@@ -0,0 +1,83 @@
+package com.ssafy.herehear.like.service;
+
+import com.ssafy.herehear.achievement.repository.MemberRepository;
+import com.ssafy.herehear.entity.*;
+import com.ssafy.herehear.global.exception.CustomException;
+import com.ssafy.herehear.global.exception.ExceptionStatus;
+import com.ssafy.herehear.like.repository.LikeMusicRepository;
+import com.ssafy.herehear.music.dto.response.LikeRegisteredMusicResDto;
+import com.ssafy.herehear.music.service.RegisteredMusicService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class LikeMusicService {
+
+    private final LikeMusicRepository likeMusicRepository;
+    private final RegisteredMusicService registeredMusicService;
+
+    public void registerlikeMusic(Long memberId, Long registeredMusicId){
+        log.info(registeredMusicService.logComment("좋아요 등록",memberId,registeredMusicId));
+
+        findLikeMusic(memberId, registeredMusicId).ifPresentOrElse(
+                existingLikeMusic -> {
+                    existingLikeMusic.updateCreateTime(LocalDateTime.now());
+                    likeMusicRepository.save(existingLikeMusic);
+                },
+                () -> {
+                    LikeMusic likeMusic = LikeMusic.builder()
+                            .id(registeredMusicService.findMemberMusicId(memberId,registeredMusicId))
+                            .member(registeredMusicService.findMember(memberId))
+                            .registeredMusic(registeredMusicService.findByRegisterMusic(registeredMusicId))
+                            .build();
+                    likeMusicRepository.save(likeMusic);
+                }
+        );
+
+        log.info("registerLike success");
+    }
+
+//    @Transactional
+//    public void deletelikeMusic(long memberId, long registeredMusicId){
+//        log.info(registeredMusicService.logComment("최근 들은 음악 삭제",memberId,registeredMusicId));
+//
+//        registeredMusicService.findMember(memberId);
+//
+//        findLikeMusic(memberId, registeredMusicId)
+//                .ifPresentOrElse(
+//                        musicHistoryRepository::delete,
+//                        () -> {
+//                            throw new CustomException(ExceptionStatus.NOT_FOUND_HISTORY_MUSIC);
+//                        }
+//                );
+//        log.info("deletePlayMusic success");
+//    }
+//
+//    @Transactional
+//    public List<LikeRegisteredMusicResDto> likeMusicList(long memberId){
+//        registeredMusicService.findMember(memberId);
+//
+//        List<LikeRegisteredMusicResDto> likeRegisteredMusicResDtos = musicHistoryRepositoryImpl.findByMusicHistorys(memberId).stream()
+//                .map(findRegisteredMusic -> musicHistoryMapper.toLikeRegisteredMusicResDto(
+//                        findRegisteredMusic,
+//                        registeredMusicService.findRegisteredMusicLike(memberId, findRegisteredMusic.getRegisteredMusicId()))
+//                )
+//                .toList();
+//        log.info("getMusicHistoryList: "+ likeRegisteredMusicResDtos);
+//
+//        return likeRegisteredMusicResDtos;
+//    }
+
+    public Optional<LikeMusic> findLikeMusic(long memberId, long registeredMusicId){
+        return likeMusicRepository.findById(registeredMusicService.findMemberMusicId(memberId, registeredMusicId));
+    }
+
+}
