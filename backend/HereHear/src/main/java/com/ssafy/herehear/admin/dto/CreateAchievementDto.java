441a8d5b18079e8e183887c0d1ab5d4a2afc7e3b
ChanHwan <song960705@naver.com>
2023-10-29 19:36:44 +0900

    feat: achievement 등록 dto 수정

--- backend/HereHear/src/main/java/com/ssafy/herehear/admin/dto/CreateAchievementDto.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/admin/dto/CreateAchievementDto.java
@@ -7,15 +7,13 @@ import lombok.Getter;
 public class CreateAchievementDto {
 
     private String mission;
-    private String icon;
     private String titleName;
     private String badgeName;
     private String badgeImg;
 
     @Builder
-    public CreateAchievementDto(String mission, String icon, String titleName, String borderName, String borderImg) {
+    public CreateAchievementDto(String mission, String titleName, String borderName, String borderImg) {
         this.mission = mission;
-        this.icon = icon;
         this.titleName = titleName;
         this.badgeName = borderName;
         this.badgeImg = borderImg;
