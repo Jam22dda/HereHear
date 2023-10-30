e11e95dbd9b81c17f486c1519dfaea2acfaa580c
cksghks89 <song960705@naver.com>
2023-10-30 12:47:37 +0900

    feat: api url 경로에서 /dev/api 제거

--- backend/HereHear/src/main/java/com/ssafy/herehear/admin/controller/AdminController.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/admin/controller/AdminController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/api/admin")
+@RequestMapping("/admin")
 public class AdminController {
 
     private final AdminService adminService;
