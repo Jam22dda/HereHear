5922617ead470d6486c17826a123810aed6be8cf
김서영 <qkdxks0922@gmail.com>
2023-10-30 10:24:11 +0900

    feat: 아토믹 디자인 atom 제작

--- frontend/HereHear/src/App.tsx
+++ frontend/HereHear/src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css';
+import "./App.css";
 
 export default function App() {
     return (
-        <div id='display'>
-            <div className='container'>
+        <div id="display">
+            <div className="container">
                 <h1>App</h1>
             </div>
         </div>
