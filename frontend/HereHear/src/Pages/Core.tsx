5347acd73189adafaff361b37318819946f983c3
AngelPlayer <ssh5212@naver.com>
2023-10-31 11:11:45 +0900

    feat: 내 위치 핀 찍기

--- frontend/HereHear/src/Pages/Core.tsx
+++ frontend/HereHear/src/Pages/Core.tsx
@@ -5,6 +5,8 @@ import testImage from '../assets/Core/Union.png';
 
 export default function Core() {
     const [isUpdate, setIsUpdate] = useState(false);
+    const [lat, setLat] = useState(0); // 위도
+    const [lng, setLng] = useState(0); // 경도
     const [musicList, setMusicList] = useState([
         {
             registeredMusicId: 3,
@@ -27,10 +29,14 @@ export default function Core() {
     ]);
 
     let naver: any;
+    let map: typeof naver;
+    // let userPin: any;
+    let script: any;
+    const [userPin, setUserPin] = useState('');
 
     useEffect(() => {
         const apiKey = import.meta.env.VITE_NAVER_MAP_API_KEY;
-        const script = document.createElement('script');
+        script = document.createElement('script');
         script.type = 'text/javascript';
         script.src = `https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${apiKey}`;
 
@@ -43,11 +49,11 @@ export default function Core() {
                 zoom: 10,
             };
 
-            const map = new naver.maps.Map('map', mapOptions);
+            map = new naver.maps.Map('map', mapOptions);
 
             const arr: any[] = [];
 
-            for (let i = 0; i < 1; i++) {
+            for (let i = 0; i < 3; i++) {
                 // 마커 표시
                 arr[i] = new naver.maps.Marker({
                     position: new naver.maps.LatLng(37.4867995957995 + i * 0.1, 126.982211871752),
@@ -59,7 +65,7 @@ export default function Core() {
                             <img
                             src="https://image.bugsm.co.kr/album/images/500/204598/20459847.jpg"
                             alt="pin-album"
-                            style="position: absolute; width: 40px; height: 40px; border-radius: 10px; left: 5px; top: 5px"
+                            style="position: absolute; width: 40px; height: 40px; border-radius: 10px; left: 5.5px; top: 5.5px"
                             />
                         </div>
                         `,
@@ -68,12 +74,52 @@ export default function Core() {
 
                 // 마커 클릭 시 발생하는 이벤트
                 naver.maps.Event.addListener(arr[i], 'click', function () {
+                    console.log(`marker${i} clicked`);
                     alert(`marker${i} clicked`);
                 });
+                console.log(arr.length);
             }
+
+            if (!navigator.geolocation) {
+                console.error('Geolocation is not supported by your browser');
+                return;
+            }
+
+            // 최초에 지도에 현재 위치 찍기
+            navigator.geolocation.getCurrentPosition(
+                position => {
+                    const { latitude, longitude } = position.coords;
+                    console.log('Latitude:', latitude, 'Longitude:', longitude);
+                    setLat(latitude);
+                    setLng(longitude);
+
+                    setUserPin(
+                        new naver.maps.Marker({
+                            position: new naver.maps.LatLng(37.4867995957995, 126.983211871752),
+                            map: map,
+                            icon: {
+                                content: `
+                                <div style="width: 30px; height: 30px; background-color: blue; border-radius: 100%; border: 4px solid white;"></div>
+                        `,
+                            },
+                        })
+                    );
+                },
+                error => {
+                    console.error('Error getting location:', error);
+                },
+                {
+                    enableHighAccuracy: true,
+                    maximumAge: 0,
+                }
+            );
+
+            // 지도 핀찍기 테스트
             setTimeout(() => {
-                const hello = new naver.maps.Marker({
-                    position: new naver.maps.LatLng(37.4867995957995, 126.972211871752),
+                arr[0].setMap(null);
+
+                arr[arr.length] = new naver.maps.Marker({
+                    position: new naver.maps.LatLng(37.4867995957995, 126.983211871752),
                     map: map,
                     icon: {
                         content: `
@@ -82,29 +128,28 @@ export default function Core() {
                             <img
                             src="https://image.bugsm.co.kr/album/images/500/204598/20459847.jpg"
                             alt="pin-album"
-                            style="position: absolute; width: 40px; height: 40px; border-radius: 10px; left: 5px; top: 5px"
+                            style="position: absolute; width: 40px; height: 40px; border-radius: 10px; left: 5.5px; top: 5.5px"
                             />
                         </div>
 
                         `,
                     },
                 });
-                arr[0].setMap(null);
+
+                setUserPin(
+                    new naver.maps.Marker({
+                        position: new naver.maps.LatLng(37.4867995957995, 126.983211871752),
+                        map: map,
+                        icon: {
+                            content: `
+                            <div style="width: 30px; height: 30px; background-color: blue; border-radius: 100%; border: 4px solid white;"></div>
+                    `,
+                        },
+                    })
+                );
+
+                console.log(arr.length);
             }, 10000); // 10초 후에 실행
-
-            // setTimeout(() => {
-            //     const hello = new naver.maps.Marker({
-            //         position: new naver.maps.LatLng(37.4867995957995, 126.972211871752),
-            //         map: map,
-            //         icon: {
-            //             content: `
-
-            //             <img alt='img' src='${testImage}' />
-            //             `,
-            //         },
-            //     });
-            //     arr[0].setMap(null);
-            // }, 11000); // 10초 후에 실행
         };
 
         document.body.appendChild(script);
