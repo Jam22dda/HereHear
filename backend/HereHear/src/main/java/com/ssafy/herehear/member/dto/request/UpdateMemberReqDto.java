55abb3f9ed717875cca15e3100bc89e0681b6c47
김주하 <tkfkd122@naver.com>
2023-11-01 14:56:10 +0900

    feat: update member request dto 추가

---
+++ backend/HereHear/src/main/java/com/ssafy/herehear/member/dto/request/UpdateMemberReqDto.java
@@ -0,0 +1,12 @@
+package com.ssafy.herehear.member.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UpdateMemberReqDto {
+    private String nickname;
+}
