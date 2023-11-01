3d911678e16dfa7e629d1b4ca3bf13210e343ab8
김주하 <tkfkd122@naver.com>
2023-11-01 14:53:25 +0900

    feat: auth refresh 기능 추가

---
+++ backend/HereHear/src/main/java/com/ssafy/herehear/member/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.ssafy.herehear.member.controller;
+
+import com.ssafy.herehear.global.exception.CustomException;
+import com.ssafy.herehear.global.exception.ExceptionStatus;
+import com.ssafy.herehear.global.response.DataResponse;
+import com.ssafy.herehear.global.util.CookieUtil;
+import com.ssafy.herehear.global.util.JwtProvider;
+import com.ssafy.herehear.member.service.AuthService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/refresh")
+    public DataResponse renewAccessToken(HttpServletRequest request) {
+        String newAccessToken = authService.refresh(request);
+        return new DataResponse("200", "토큰 발급이 완료되었습니다", newAccessToken);
+    }
+
+}
