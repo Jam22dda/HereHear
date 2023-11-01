7cea214ce7d01e4228ec7c91314a1d330fa829c6
김서영 <qkdxks0922@gmail.com>
2023-11-01 17:36:26 +0900

    feat: 페이지 라우팅 및 아이콘 추가

--- frontend/HereHear/src/Pages/LikePage/LikePage.tsx
+++ frontend/HereHear/src/Pages/LikePage/LikePage.tsx
@@ -6,15 +6,35 @@ import iconEmptyheart from "../../../public/images/icon-emptyheart.png";
 import { Image } from "../../components/atoms/Image/Image";
 import { Text } from "../../components/atoms/Text/Text.styles";
 import BTS_answer from "../../../public/images/BTS_answer.jpg";
+import iconBack from "../../../public/images/icon-back.png";
+import { useNavigate } from "react-router-dom";
 
 export default function LikePage() {
+    const navigate = useNavigate(); // useNavigate 훅 사용
+
+    const navigatePage = (path: string) => {
+        navigate(path);
+    };
+
     return (
         <div id="display">
             <div className="container">
+                <CircleButton
+                    option="default2"
+                    size="medium"
+                    onClick={() => navigatePage("/mypage/1")}
+                >
+                    <Image
+                        src={iconBack}
+                        width={10}
+                        height={18}
+                        $unit="px"
+                    ></Image>
+                </CircleButton>
                 <Text
                     size="subtitle1"
                     fontWeight="bold"
-                    $margin="50px 0 30px 0"
+                    $margin="30px 0 50px 0"
                 >
                     내가 좋아요한 노래
                 </Text>
---
+++ frontend/HereHear/src/Pages/MyStatisticsPage/MyStatisticsPage.tsx
@@ -0,0 +1,9 @@
+export default function MyStatisticsPage() {
+    return (
+        <div id="display">
+            <div className="container">
+                <h1>Follow</h1>
+            </div>
+        </div>
+    );
+}
