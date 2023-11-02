74273ff5066ffe24e1ca8737c5ec2165783be919
samsung <gkathaud@naver.com>
2023-11-02 10:38:37 +0900

    feat: SSE 추가

--- backend/HereHear/src/main/java/com/ssafy/herehear/music/controller/RegisteredMusicController.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/music/controller/RegisteredMusicController.java
@@ -2,12 +2,11 @@ package com.ssafy.herehear.music.controller;
 
 import com.ssafy.herehear.global.response.CommonResponse;
 import com.ssafy.herehear.global.response.DataResponse;
-import com.ssafy.herehear.music.dto.request.AroundMusicReqDto;
-import com.ssafy.herehear.music.dto.request.AroundSearchReqDto;
-import com.ssafy.herehear.music.dto.request.MusicRegisteredIdReqDto;
+import com.ssafy.herehear.music.dto.request.UpdateMusicIdReqDto;
 import com.ssafy.herehear.music.dto.request.RegisterMusicReqDto;
 import com.ssafy.herehear.music.dto.response.*;
 import com.ssafy.herehear.music.service.RegisteredMusicService;
+import com.ssafy.herehear.music.service.SseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,16 +18,18 @@ import java.util.List;
 public class RegisteredMusicController {
 
     private final RegisteredMusicService registeredMusicService;
+    private final SseService sseService;
 
     @PostMapping
     public CommonResponse registerMusic(@RequestHeader("Member-id") Long memberId, @RequestBody RegisterMusicReqDto req) {
-        registeredMusicService.registerMusic(memberId, req);
+        SseResDto sseResDto = registeredMusicService.registerMusic(memberId, req);
+        sseService.notify(memberId, sseResDto);
         return new CommonResponse("200", "음악 등록");
     }
 
     @GetMapping("/tag")
     public DataResponse<List<OccasionResDto>> readTag() {
-        return new DataResponse<>("200", "전체 태그 조회", registeredMusicService.getTag());
+        return new DataResponse<>("200", "전체 태그 조회", registeredMusicService.getAllTags());
     }
 
     @GetMapping("/{registeredMusicId}")
@@ -36,30 +37,21 @@ public class RegisteredMusicController {
         return new DataResponse<>("200", "음악 상세 조회", registeredMusicService.getRegisteredMusicDetails(memberId,registeredMusicId));
     }
 
+    @PutMapping
+    public CommonResponse updateMyRegisteredMusic(@RequestHeader("Member-id") Long memberId, @RequestBody UpdateMusicIdReqDto req) {
+        SseResDto sseResDto = registeredMusicService.updateMyRegisteredMusic(memberId, req.getRegisteredMusicId());
+        sseService.notify(memberId, sseResDto);
+        return new CommonResponse("200", "등록한 음악 삭제");
+    }
+
     @GetMapping("/list")
     public DataResponse<List<RegisteredMusicResDto>> registeredMusicList() {
         return new DataResponse<>("200", "전체 음악 조회", registeredMusicService.getRegisteredMusicList());
     }
 
-    @PutMapping("/my/list")
-    public CommonResponse updateMyRegisteredMusic(@RequestHeader("Member-id") Long memberId, @RequestBody MusicRegisteredIdReqDto req) {
-        registeredMusicService.updateMyRegisteredMusic(memberId, req.getRegisteredMusicId());
-        return new CommonResponse("200", "등록한 음악 삭제");
-    }
-
     @GetMapping("/my/list")
     public DataResponse<List<MyRegisteredMusicResDto>> myRegisteredMusicList(@RequestHeader("Member-id") Long memberId) {
         return new DataResponse<>("200", "내가 등록한 음악 조회", registeredMusicService.getMyRegisteredMusicList(memberId));
-    }
-
-    @GetMapping("/around/list")
-    public DataResponse<List<AroundMusicResDto>> aroundMusicList(@RequestBody AroundMusicReqDto aroundMusicReqDto) {
-        return new DataResponse<>("200", "주변 음악 조회", registeredMusicService.getAroundMusicList(aroundMusicReqDto));
-    }
-
-    @GetMapping("/around/search")
-    public DataResponse<List<AroundMusicResDto>> aroundSearchMusic(@RequestBody AroundSearchReqDto aroundSearchReqDto) {
-        return new DataResponse<>("200", "주변 음악 검색", registeredMusicService.getAroundSearchMusic(aroundSearchReqDto));
     }
 
 }
---
+++ backend/HereHear/src/main/java/com/ssafy/herehear/music/dto/response/SseResDto.java
@@ -0,0 +1,21 @@
+package com.ssafy.herehear.music.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Builder
+@ToString
+public class SseResDto {
+    private int status;//0:삭제//1:등록
+    private Long registeredMusicId;
+    private Double lng;
+    private Double lat;
+    private String subject;
+    private String singer;
+    private String albumImg;
+    private List<String> occasionName;
+}
