34ad13ad36fcde8871f01e4712e79d6724aa9beb
김주하 <tkfkd122@naver.com>
2023-11-03 08:59:31 +0900

    modify: lombok 라이브러리 삭제

--- backend/HereHear/src/main/java/com/ssafy/herehear/member/dto/request/SignUpReqDto.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/member/dto/request/SignUpReqDto.java
@@ -3,9 +3,6 @@ package com.ssafy.herehear.member.dto.request;
 import lombok.*;
 
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class SignUpReqDto {
     private Long memberId;
     private String nickname;
