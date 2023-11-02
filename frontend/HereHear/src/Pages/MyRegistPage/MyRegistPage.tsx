5b138deca4c747a551951afcaafd32f0a3322260
seol <seolhyehyeon@gmail.com>
2023-11-02 10:09:15 +0900

    feature:버튼이미지추가

--- frontend/HereHear/src/Pages/MyRegistPage/MyRegistPage.tsx
+++ frontend/HereHear/src/Pages/MyRegistPage/MyRegistPage.tsx
@@ -3,7 +3,7 @@ import MusicItem from "../../components/molcules/MusicItem/MusicItem";
 import { Text } from "../../components/atoms/Text/Text.styles";
 import BTS_answer from "../../../public/images/BTS_answer.jpg";
 import { useNavigate } from "react-router-dom";
-import iconBack from "../../../public/images/icon-back.png";
+import iconBack from "../../assets/CircleButton/icon-back.png";
 import CircleButton from "../../components/atoms/CircleButton/CircleButton";
 import { Image } from "../../components/atoms/Image/Image";
 
@@ -17,38 +17,17 @@ export default function MyRegistPage() {
     return (
         <div id="display">
             <div className="container">
-                <CircleButton
-                    option="default2"
-                    size="medium"
-                    onClick={() => navigatePage("/mypage/1")}
-                >
-                    <Image
-                        src={iconBack}
-                        width={10}
-                        height={18}
-                        $unit="px"
-                    ></Image>
+                <CircleButton option="default2" size="medium" onClick={() => navigatePage("/mypage/1")}>
+                    <Image src={iconBack} width={10} height={18} $unit="px"></Image>
                 </CircleButton>
-                <Text
-                    size="subtitle1"
-                    fontWeight="bold"
-                    $margin="50px 0 30px 0"
-                >
+                <Text size="subtitle1" fontWeight="bold" $margin="50px 0 30px 0">
                     내가 등록한 노래
                 </Text>
                 <S.MyRegistWrapper>
-                    <MusicItem
-                        src={BTS_answer}
-                        title="Answer : Love Myself"
-                        artist="방탄소년단"
-                    ></MusicItem>
+                    <MusicItem src={BTS_answer} title="Answer : Love Myself" artist="방탄소년단"></MusicItem>
                 </S.MyRegistWrapper>
                 <S.MyRegistWrapper>
-                    <MusicItem
-                        src={BTS_answer}
-                        title="Answer : Love Myself"
-                        artist="방탄소년단"
-                    ></MusicItem>
+                    <MusicItem src={BTS_answer} title="Answer : Love Myself" artist="방탄소년단"></MusicItem>
                 </S.MyRegistWrapper>
             </div>
         </div>
