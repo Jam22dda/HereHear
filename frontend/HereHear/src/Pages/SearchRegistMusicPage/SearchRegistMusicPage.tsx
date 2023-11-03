10cbb09fa603d326d29c8486c645da82a02ca978
seol <seolhyehyeon@gmail.com>
2023-11-03 10:54:40 +0900

    fix :위치버튼 수정, 음악검색 수정

--- frontend/HereHear/src/Pages/SearchRegistMusicPage/SearchRegistMusicPage.tsx
+++ frontend/HereHear/src/Pages/SearchRegistMusicPage/SearchRegistMusicPage.tsx
@@ -4,12 +4,12 @@ import React, { useState, ChangeEvent } from "react";
 import CircleButton from "../../components/atoms/CircleButton/CircleButton";
 import { Text } from "../../components/atoms/Text/Text.styles";
 import Input from "../../components/atoms/Input/Input";
-import MusicItem from "../../components/molcules/MusicItem/MusicItem";
+// import MusicItem from "../../components/molcules/MusicItem/MusicItem";
 import Button from "../../components/atoms/Button/Button";
 import { useGetSearchMusic } from "../../apis/Music/Quries/useGetSearchMusic";
-import { MusicData } from "../../types/music";
+// import { MusicData } from "../../types/music";
 import iconBack from "../../assets/CircleButton/icon-back.png";
-import iconMusicPlus from "../../assets/CircleButton/icon-musicPlus.png";
+// import iconMusicPlus from "../../assets/CircleButton/icon-musicPlus.png";
 import { Image } from "../../components/atoms/Image/Image";
 
 export default function SearchRegistMusic() {
@@ -19,28 +19,21 @@ export default function SearchRegistMusic() {
     // startTransition을 사용하여 상태 업데이트를 비동기적으로 처리
     // const [, startTransition] = useTransition();
     const [searchKeyword, setSearchKeyword] = useState("");
+    const [temp, setTemp] = useState("");
 
-    const [searchResults, setSearchResults] = useState<MusicData[] | null>(
-        null
-    );
-    const { searchMusic } = useGetSearchMusic(searchKeyword, page);
-    console.log(searchMusic);
+    // const [searchResults, setSearchResults] = useState<MusicData[] | null>(null);
+    const { searchMusic } = useGetSearchMusic(temp, page);
+
     const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
         setSearchKeyword(e.target.value);
     };
-    console.log(searchKeyword);
-
+    console.log(searchMusic, "음악가져왔니?");
     const handleSearchClick = () => {
-        if (searchKeyword) {
-            if (
-                searchMusic.searchMusic &&
-                searchMusic.searchMusic.data &&
-                searchMusic.searchMusic.data.musicInfoList
-            ) {
-                console.log("야 오냐?");
-                setSearchResults(searchMusic.searchMusic.data.musicInfoList);
-            }
-        }
+        console.log("야 오냐?");
+        setTemp(searchKeyword);
+        // setSearchResults(searchMusic.data.musicInfoList);
+        // setSearchResults(searchMusic.data.musicInfoList);
+        // console.log(searchMusic.data.musicInfoList, "음악결과");
     };
 
     // const handleSearchClick = () => {
@@ -61,17 +54,8 @@ export default function SearchRegistMusic() {
     return (
         <div id="display">
             <div className="container">
-                <CircleButton
-                    option="default2"
-                    size="medium"
-                    onClick={() => navigate(-1)}
-                >
-                    <Image
-                        src={iconBack}
-                        width={10}
-                        height={18}
-                        $unit="px"
-                    ></Image>
+                <CircleButton option="default2" size="medium" onClick={() => navigate(-1)}>
+                    <Image src={iconBack} width={10} height={18} $unit="px"></Image>
                 </CircleButton>
                 <Text size="subtitle1" fontWeight="bold" $marginTop="20px">
                     음악 등록
@@ -83,41 +67,22 @@ export default function SearchRegistMusic() {
                         onChange={handleSearchChange}
                         // disabled={isLoading} // 로딩 중일 때 입력창 비활성화
                     ></Input>
-                    <Button
-                        option="follow"
-                        $height="44px"
-                        $width="60px"
-                        $borderRadius="12px"
-                        color="white1"
-                        size="mediumplus"
-                        onClick={handleSearchClick}
-                    >
+                    <Button option="follow" $height="44px" $width="60px" $borderRadius="12px" color="white1" size="mediumplus" onClick={handleSearchClick}>
                         검색
                     </Button>
                 </S.InputWrapper>
 
-                {searchResults &&
+                {/* {searchResults &&
                     searchResults.map((item, index) => (
                         <S.MusicItemWrapper key={index}>
-                            <MusicItem
-                                src={item.album_images[0].url}
-                                title={item.subject}
-                                artist={item.artists[0].name}
-                            />
-                            <CircleButton
-                                option="gradDeActivated"
-                                size="mediumplus"
-                            >
-                                <Image
-                                    src={iconMusicPlus}
-                                    width={24}
-                                    height={24}
-                                    $unit="px"
-                                ></Image>
-                            </CircleButton>
-                            {/* TODO: 버튼 클릭이벤트- 음악 저장하고 음악 등록페이지로 이동 */}
-                        </S.MusicItemWrapper>
-                    ))}
+                            <MusicItem src={item.album_images[0].url} title={item.subject} artist={item.artists[0].name} />
+                            <CircleButton option="gradDeActivated" size="mediumplus">
+                                <Image src={iconMusicPlus} width={24} height={24} $unit="px"></Image>
+                            </CircleButton> */}
+
+                {/* TODO: 버튼 클릭이벤트- 음악 저장하고 음악 등록페이지로 이동 */}
+                {/* </S.MusicItemWrapper>
+                    ))} */}
             </div>
         </div>
     );
