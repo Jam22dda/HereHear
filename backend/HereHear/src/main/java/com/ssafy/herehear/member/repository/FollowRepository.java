8eb38f9faf841ab4f8d02595f63ccd8eb6938790
cksghks89 <song960705@naver.com>
2023-11-03 13:44:19 +0900

    fix: follow repository @Param 추가

--- backend/HereHear/src/main/java/com/ssafy/herehear/member/repository/FollowRepository.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/member/repository/FollowRepository.java
@@ -3,6 +3,7 @@ package com.ssafy.herehear.member.repository;
 import com.ssafy.herehear.entity.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,12 +11,12 @@ import java.util.Optional;
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
     @Query("select f from Follow f join fetch f.member m where f.followMemberId = :memberId")
-    List<Follow> findByFollowerMemberId(Long memberId);
+    List<Follow> findByFollowerMemberId(@Param("memberId") Long memberId);
 
     @Query("select f from Follow f where f.member.memberId = :memberId")
-    List<Follow> findByFollowingMemberId(Long memberId);
+    List<Follow> findByFollowingMemberId(@Param("memberId") Long memberId);
 
     @Query("select f from Follow f where f.member.memberId = :memberId and f.followMemberId = :followingMemberId")
-    Optional<Follow> findByMemberIdAndFollowMemberId(Long memberId, Long followingMemberId);
+    Optional<Follow> findByMemberIdAndFollowMemberId(@Param("memberId") Long memberId, @Param("followingMemberId") Long followingMemberId);
 
 }
