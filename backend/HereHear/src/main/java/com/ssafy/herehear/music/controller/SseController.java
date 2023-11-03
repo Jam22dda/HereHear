714c2075f55dc3c21ecfa9b65e2c380879ffed27
samsung <gkathaud@naver.com>
2023-11-03 14:37:23 +0900

    feat: 등록 및 삭제 리스트 SSE 추가

--- backend/HereHear/src/main/java/com/ssafy/herehear/music/controller/SseController.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/music/controller/SseController.java
@@ -1,5 +1,6 @@
 package com.ssafy.herehear.music.controller;
 
+import com.ssafy.herehear.global.response.CommonResponse;
 import com.ssafy.herehear.music.service.SseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -19,7 +20,10 @@ public class SseController {
     @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public SseEmitter subscribe(Authentication authentication) {
         Long memberId = Long.parseLong(authentication.getName());
-        return sseService.subscribe(memberId);
+        SseEmitter sseEmitter = sseService.subscribe(memberId);
+        sseService.sendToClient(Long.parseLong(authentication.getName()), new CommonResponse("200", "SSE 구독완료"));
+
+        return sseEmitter;
     }
 
 }
