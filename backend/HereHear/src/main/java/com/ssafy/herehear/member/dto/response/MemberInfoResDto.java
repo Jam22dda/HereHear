c0c6b8ba89cf2373ad409f69ac8f0ea044644aa2
김주하 <tkfkd122@naver.com>
2023-11-03 09:00:31 +0900

    modify: Entity 변수 dto로 변경

--- backend/HereHear/src/main/java/com/ssafy/herehear/member/dto/response/MemberInfoResDto.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/member/dto/response/MemberInfoResDto.java
@@ -9,5 +9,5 @@ import lombok.*;
 @AllArgsConstructor
 public class MemberInfoResDto {
     private String nickname;
-    private ProfileCharacter profileCharacter;
+    private ProfileCharacterResDto profileCharacter;
 }
