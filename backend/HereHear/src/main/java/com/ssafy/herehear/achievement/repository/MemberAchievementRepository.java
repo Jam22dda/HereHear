1b70d21d2ee0297e2fbb6fa615d853c7635eff8f
cksghks89 <song960705@naver.com>
2023-11-03 11:01:10 +0900

    fix: jpa repository 메서드에 @Param 추가

--- backend/HereHear/src/main/java/com/ssafy/herehear/achievement/repository/MemberAchievementRepository.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/achievement/repository/MemberAchievementRepository.java
@@ -3,13 +3,14 @@ package com.ssafy.herehear.achievement.repository;
 import com.ssafy.herehear.entity.MemberAchievement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberAchievementRepository extends JpaRepository<MemberAchievement, Long> {
 
     @Query("select ma from MemberAchievement ma join fetch ma.achievement where ma.member.memberId = :memberId")
-    List<MemberAchievement> findByMemberId(Long memberId);
+    List<MemberAchievement> findByMemberId(@Param("memberId") Long memberId);
 
     int countByMember_MemberId(Long memberId);
 
