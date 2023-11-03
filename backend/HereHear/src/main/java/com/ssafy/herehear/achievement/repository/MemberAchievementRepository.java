5b75419eda08185c61b250d4d23de33f03a7f169
cksghks89 <song960705@naver.com>
2023-11-03 13:40:37 +0900

    feat: 달성한 업적 조회시 전체목록에서 내가 달성한 것 표시하여 반환

--- backend/HereHear/src/main/java/com/ssafy/herehear/achievement/repository/MemberAchievementRepository.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/achievement/repository/MemberAchievementRepository.java
@@ -9,7 +9,9 @@ import java.util.List;
 
 public interface MemberAchievementRepository extends JpaRepository<MemberAchievement, Long> {
 
-    @Query("select ma from MemberAchievement ma join fetch ma.achievement where ma.member.memberId = :memberId")
+    @Query("select ma from MemberAchievement ma join fetch ma.achievement " +
+            "join fetch ma.achievement.titleCode " +
+            "join fetch ma.achievement.badgeCode where ma.member.memberId = :memberId")
     List<MemberAchievement> findByMemberId(@Param("memberId") Long memberId);
 
     int countByMember_MemberId(Long memberId);
