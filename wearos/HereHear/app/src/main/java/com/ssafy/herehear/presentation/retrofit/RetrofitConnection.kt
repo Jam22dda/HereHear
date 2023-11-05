2bbb8c178d8000cd6ac48cda61a83d68d4966970
ChanHwan <song960705@naver.com>
2023-11-06 03:23:50 +0900

    feat: Retrofit API 호출부 및 객체 구현

--- wearos/HereHear/app/src/main/java/com/ssafy/herehear/presentation/retrofit/RetrofitConnection.kt
+++ wearos/HereHear/app/src/main/java/com/ssafy/herehear/presentation/retrofit/RetrofitConnection.kt
@@ -1,23 +1,39 @@
 package com.ssafy.herehear.presentation.retrofit
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitConnection {
 
     companion object {
-        private const val BASE_URL = "http://10.0.2.2:8080"
+//        local URL
+//        private const val BASE_URL = "http://10.0.2.2:8080"
+
+        // server URL
+        private const val BASE_URL = "https:/k9b202.p.ssafy.io/api/"
+
         private var INSTANCE: Retrofit? = null
 
         fun getInstance(): Retrofit {
             if (INSTANCE == null) {
+//                val okHttpClient = OkHttpClient.Builder()
+//                    .addInterceptor { chain ->
+//                        val originalRequest = chain.request()
+//                        val newRequest = originalRequest.newBuilder()
+//                            .header("personalCode", "1234")
+//                            .build()
+//                        chain.proceed(newRequest)
+//                    }
+//                    .build()
+
                 INSTANCE = Retrofit.Builder()
                     .baseUrl(BASE_URL)
+//                    .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build()
             }
             return INSTANCE!!
         }
     }
-
 }
