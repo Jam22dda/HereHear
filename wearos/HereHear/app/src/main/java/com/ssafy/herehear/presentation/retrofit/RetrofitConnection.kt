ba26546b148598c9ca21ed71b3f1dcb459cb271b
ChanHwan <song960705@naver.com>
2023-11-05 12:46:20 +0900

    feat: retrofit connection 객체 싱글톤으로 생성하는 클래스 구현

---
+++ wearos/HereHear/app/src/main/java/com/ssafy/herehear/presentation/retrofit/RetrofitConnection.kt
@@ -0,0 +1,23 @@
+package com.ssafy.herehear.presentation.retrofit
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitConnection {
+
+    companion object {
+        private const val BASE_URL = "http://10.0.2.2:8080"
+        private var INSTANCE: Retrofit? = null
+
+        fun getInstance(): Retrofit {
+            if (INSTANCE == null) {
+                INSTANCE = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            }
+            return INSTANCE!!
+        }
+    }
+
+}
