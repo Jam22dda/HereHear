6248b779e2a1db94218d5ca49027a213a524e643
ChanHwan <song960705@naver.com>
2023-11-06 03:22:43 +0900

    feat: 인증코드 전송 및 검증 API 구현

--- wearos/HereHear/app/src/main/java/com/ssafy/herehear/presentation/retrofit/api/PersonalCodeApi.kt
+++ wearos/HereHear/app/src/main/java/com/ssafy/herehear/presentation/retrofit/api/PersonalCodeApi.kt
@@ -1,12 +1,15 @@
 package com.ssafy.herehear.presentation.retrofit.api
 
+import android.content.Context
 import android.util.Log
 import androidx.navigation.NavHostController
 import androidx.navigation.NavOptions
-import com.ssafy.herehear.R
 import com.ssafy.herehear.presentation.retrofit.RetrofitConnection
+import com.ssafy.herehear.presentation.retrofit.data.response.ApiResponse
 import com.ssafy.herehear.presentation.retrofit.data.response.AuthResponse
 import com.ssafy.herehear.presentation.retrofit.service.PersonalCodeService
+import com.ssafy.herehear.presentation.util.deletePersonalCodeFile
+import com.ssafy.herehear.presentation.util.writePersonalCodeFile
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -14,21 +17,24 @@ import retrofit2.Response
 fun personalCodeApi(
     personalCode: String,
     setInputCode: (String) -> Unit,
-    navController: NavHostController
+    navController: NavHostController,
+    baseContext: Context
 ) {
     val retrofitAPI = RetrofitConnection.getInstance().create(PersonalCodeService::class.java)
 
     retrofitAPI.authPersonalCode(personalCode)
-        .enqueue(object : Callback<AuthResponse> {
+        .enqueue(object : Callback<ApiResponse> {
             override fun onResponse(
-                call: Call<AuthResponse>,
-                response: Response<AuthResponse>
+                call: Call<ApiResponse>,
+                response: Response<ApiResponse>
             ) {
-                val result: AuthResponse = response.body() ?: AuthResponse(false, "", -1)
+                Log.d("Retrofit", "personal code api 호출 성공")
+                Log.d("Retrofit", response.body().toString())
+                val result: ApiResponse? = response.body()
 
-                if (result.accessResult) {
-                    Log.d("response personalCode", result.personalCode)
-                    Log.d("response userId", result.userId.toString())
+                if (result?.data?.accessResult == true) {
+                    baseContext.deletePersonalCodeFile("personalCode.txt")
+                    baseContext.writePersonalCodeFile("personalCode.txt", result.data.personalCode)
 
                     val navOptions = NavOptions.Builder()
                         .setPopUpTo("landing", true)
@@ -39,7 +45,7 @@ fun personalCodeApi(
                 }
             }
 
-            override fun onFailure(call: Call<AuthResponse>, t: Throwable) {
+            override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
                 Log.d("Retrofit", "personal code api 호출 실패")
             }
         })
