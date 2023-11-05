3b125009daa45a22b108bf2fafd976ba116f1c18
ChanHwan <song960705@naver.com>
2023-11-06 01:29:46 +0900

    feat: Wear os personalCode filter 추가 및 구현

---
+++ backend/HereHear/src/main/java/com/ssafy/herehear/member/repository/WearOsPersonalCodeRepository.java
@@ -0,0 +1,10 @@
+package com.ssafy.herehear.member.repository;
+
+import com.ssafy.herehear.entity.WearOsPersonalCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface WearOsPersonalCodeRepository extends JpaRepository<WearOsPersonalCode, Long> {
+    Optional<WearOsPersonalCode> findByPersonalCode(String personalCode);
+}
