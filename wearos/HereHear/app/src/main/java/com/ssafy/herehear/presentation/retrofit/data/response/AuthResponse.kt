ad6c9b4c2a16cdbcb7b026294714bb3037815910
ChanHwan <song960705@naver.com>
2023-11-06 03:22:58 +0900

    feat: 인증코드 응답을 위한 data class 구현

--- wearos/HereHear/app/src/main/java/com/ssafy/herehear/presentation/retrofit/data/response/AuthResponse.kt
+++ wearos/HereHear/app/src/main/java/com/ssafy/herehear/presentation/retrofit/data/response/AuthResponse.kt
@@ -1,5 +1,11 @@
 package com.ssafy.herehear.presentation.retrofit.data.response
 
+data class ApiResponse(
+    val code: String,
+    val message: String,
+    val data: AuthResponse
+)
+
 data class AuthResponse(
     val accessResult: Boolean,
     val personalCode: String,
