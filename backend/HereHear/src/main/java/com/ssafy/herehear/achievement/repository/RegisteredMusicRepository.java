a25afde199ea4d77903e5ff93e8711d266214839
ChanHwan <song960705@naver.com>
2023-11-05 22:15:01 +0900

    feat: 한 곡의 누적 좋아요 개수를 세는 JPA 메서드 구현

--- backend/HereHear/src/main/java/com/ssafy/herehear/achievement/repository/RegisteredMusicRepository.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/achievement/repository/RegisteredMusicRepository.java
@@ -1,5 +1,6 @@
 package com.ssafy.herehear.achievement.repository;
 
+import com.ssafy.herehear.entity.Member;
 import com.ssafy.herehear.entity.RegisteredMusic;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -17,6 +18,4 @@ public interface RegisteredMusicRepository extends JpaRepository<RegisteredMusic
 
     Optional<RegisteredMusic> findByRegisteredMusicId(Long registeredMusicId);
 
-    @Query("select rm from RegisteredMusic rm join fetch rm.member where rm.member.memberId = :memberId and rm.isDeleted = null")
-    List<RegisteredMusic> findByMember_MemberId(@Param("memberId") Long memberId);
 }
