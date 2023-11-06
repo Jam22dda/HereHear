38015d7b94a5bd5ac81c4f337cee493dde255378
김주하 <tkfkd122@naver.com>
2023-11-06 15:51:06 +0900

    modify: 필요 없는 라이브러리 삭제

--- backend/HereHear/src/main/java/com/ssafy/herehear/member/dto/request/UpdateMemberReqDto.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/member/dto/request/UpdateMemberReqDto.java
@@ -1,12 +1,8 @@
 package com.ssafy.herehear.member.dto.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
-@AllArgsConstructor
 public class UpdateMemberReqDto {
     private String nickname;
 }
