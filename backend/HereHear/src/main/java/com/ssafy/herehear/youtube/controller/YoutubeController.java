64900a4636b4c67ec655b838e7398fe90a0671df
samsung <gkathaud@naver.com>
2023-11-07 16:40:19 +0900

    feat: YoutubeController 추가

---
+++ backend/HereHear/src/main/java/com/ssafy/herehear/youtube/controller/YoutubeController.java
@@ -0,0 +1,27 @@
+package com.ssafy.herehear.youtube.controller;
+
+import com.ssafy.herehear.global.response.CommonResponse;
+import com.ssafy.herehear.youtube.dto.YoutubePlayReqDto;
+import com.ssafy.herehear.youtube.service.AddPlayItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+@RestController
+@RequestMapping("/youtube")
+@RequiredArgsConstructor
+public class YoutubeController {
+
+    private final AddPlayItemService addPlayItemService;
+
+    @PostMapping
+    public CommonResponse insertList(@RequestBody YoutubePlayReqDto req) throws GeneralSecurityException, IOException {
+        addPlayItemService.selectList();
+        addPlayItemService.insertList("listId");
+        addPlayItemService.addPlayItem(req.getVideoId());
+        return new CommonResponse("200", "재생 목록 추가완료");
+    }
+
+}
