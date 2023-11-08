feb9df5975c1d37ec9b9a9e03f53cac4f69fdd0b
samsung <gkathaud@naver.com>
2023-11-08 16:33:36 +0900

    feat: sse 음악 삭제 시간 필터 추가

--- backend/HereHear/src/main/java/com/ssafy/herehear/music/util/HourFilterUtils.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/music/util/HourFilterUtils.java
@@ -1,26 +1,38 @@
 package com.ssafy.herehear.music.util;
 
 import com.ssafy.herehear.entity.RegisteredMusic;
+import com.ssafy.herehear.music.dto.response.SseResDto;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 public class HourFilterUtils {
 
     public static boolean findHourFilter(RegisteredMusic findRegisteredMusic){
         long hoursDifference = hoursDifference(findRegisteredMusic);
-        return hoursDifference > -3 && hoursDifference < 3 || hoursDifference >= 21 || hoursDifference <= -21;//-3h ~ +3h
+        return hoursDifference > -3 && hoursDifference < 3 || hoursDifference >= 21 || hoursDifference <= -21;
+    }
+
+    public static boolean checkHour(List<SseResDto> sseResDto){
+        // 현재 시간에서 '시'만 추출
+        int currentHour = LocalDateTime.now().getHour();
+        LocalTime timeWithHourOnly = LocalTime.of(currentHour, 0);
+
+        long hoursDifference = ChronoUnit.HOURS.between(timeWithHourOnly, sseResDto.get(0).getCreateTime());
+
+        return hoursDifference > -3 && hoursDifference < 3 || hoursDifference >= 21 || hoursDifference <= -21;
     }
 
     public static boolean beforeHourFilter(RegisteredMusic findRegisteredMusic){
         long hoursDifference = hoursDifference(findRegisteredMusic);
-        return hoursDifference == -3 || hoursDifference == 20;//-4h ~ -3h
+        return hoursDifference == -3 || hoursDifference == 20;
     }
 
     public static boolean afterHourFilter(RegisteredMusic findRegisteredMusic){
         long hoursDifference = hoursDifference(findRegisteredMusic);
-        return hoursDifference == 2 || hoursDifference == -21;//+3h ~ +4h
+        return hoursDifference == 2 || hoursDifference == -21;
     }
 
     public static long hoursDifference(RegisteredMusic findRegisteredMusic){
