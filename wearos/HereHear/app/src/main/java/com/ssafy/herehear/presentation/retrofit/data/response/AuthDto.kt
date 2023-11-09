07f55d0a6f681cb37a3c03e3570f2e1f0dfb454b
cksghks89 <song960705@naver.com>
2023-11-09 19:37:23 +0900

    feat: 인증시 사용할 auth dto 생성

--- wearos/HereHear/app/src/main/java/com/ssafy/herehear/presentation/retrofit/data/response/AuthDto.kt
+++ wearos/HereHear/app/src/main/java/com/ssafy/herehear/presentation/retrofit/data/response/AuthDto.kt
@@ -1,13 +1,9 @@
 package com.ssafy.herehear.presentation.retrofit.data.response
 
+import com.ssafy.herehear.presentation.data.AuthDto
+
 data class ApiResponse(
     val code: String,
     val message: String,
-    val data: AuthResponse
-)
-
-data class AuthResponse(
-    val accessResult: Boolean,
-    val personalCode: String,
-    val userId: Int,
+    val data: AuthDto
 )
