6e17871f12a3e6ac09f445d72d40a18aa311d889
김주하 <tkfkd122@naver.com>
2023-11-10 20:07:56 +0900

    modify: header 추가

--- backend/HereHear/src/main/java/com/ssafy/herehear/music/controller/SseController.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/music/controller/SseController.java
@@ -2,6 +2,7 @@ package com.ssafy.herehear.music.controller;
 
 import com.ssafy.herehear.global.response.CommonResponse;
 import com.ssafy.herehear.music.service.SseService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +19,8 @@ public class SseController {
     private final SseService sseService;
 
     @GetMapping(value = "/subscribe/{memberId}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter subscribe(@PathVariable Long memberId) {
+    public SseEmitter subscribe(@PathVariable Long memberId, HttpServletResponse response) {
+        response.setHeader("X-Accel-Buffering", "no");
         SseEmitter sseEmitter = sseService.subscribe(memberId);
         sseService.sendToClient(memberId, new CommonResponse("200", "SSE 구독완료"));
 
