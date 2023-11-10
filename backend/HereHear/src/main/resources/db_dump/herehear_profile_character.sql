96c5b2e24f9ef1ba852119badbd5ff0978b6c629
cksghks89 <song960705@naver.com>
2023-11-10 12:35:52 +0900

    feat: db dump 파일 추가

--- backend/HereHear/src/main/resources/db_dump/herehear_profile_character.sql
+++ backend/HereHear/src/main/resources/db_dump/herehear_profile_character.sql
@@ -22,11 +22,12 @@
 DROP TABLE IF EXISTS `profile_character`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `profile_character` (
-  `profile_character_id` bigint NOT NULL AUTO_INCREMENT,
-  `character_image` varchar(255) DEFAULT NULL,
-  `character_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`profile_character_id`)
+CREATE TABLE `profile_character`
+(
+    `profile_character_id` bigint NOT NULL AUTO_INCREMENT,
+    `character_image`      varchar(255) DEFAULT NULL,
+    `character_name`       varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`profile_character_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -34,11 +35,19 @@ CREATE TABLE `profile_character` (
 -- Dumping data for table `profile_character`
 --
 
-LOCK TABLES `profile_character` WRITE;
+LOCK
+TABLES `profile_character` WRITE;
 /*!40000 ALTER TABLE `profile_character` DISABLE KEYS */;
-INSERT INTO `profile_character` VALUES (1,'https://k9b202.p.ssafy.io/api/file/download/3384276135fb416caf037fcd77d91f5d.png','히어먼지'),(2,'https://k9b202.p.ssafy.io/api/file/download/a78b895e6786476894cfa8616d1875ba.png','힙합먼지'),(3,'https://k9b202.p.ssafy.io/api/file/download/212bb8db683e44a28e48c3582683c539.png','공부먼지'),(4,'https://k9b202.p.ssafy.io/api/file/download/0735acc01ef047fe929668107d0ad1eb.png','꿀잠먼지'),(5,'https://k9b202.p.ssafy.io/api/file/download/9aad9a1f5f4745ad9252ad90f60df420.png','운동먼지'),(6,'https://k9b202.p.ssafy.io/api/file/download/893e3c5bcc984f4d9e7bc11549dc3954.png','열정먼지');
+INSERT INTO `profile_character`
+VALUES (1, 'https://k9b202.p.ssafy.io/api/file/download/3384276135fb416caf037fcd77d91f5d.png', '히어먼지'),
+       (2, 'https://k9b202.p.ssafy.io/api/file/download/a78b895e6786476894cfa8616d1875ba.png', '힙합먼지'),
+       (3, 'https://k9b202.p.ssafy.io/api/file/download/212bb8db683e44a28e48c3582683c539.png', '공부먼지'),
+       (4, 'https://k9b202.p.ssafy.io/api/file/download/0735acc01ef047fe929668107d0ad1eb.png', '꿀잠먼지'),
+       (5, 'https://k9b202.p.ssafy.io/api/file/download/9aad9a1f5f4745ad9252ad90f60df420.png', '운동먼지'),
+       (6, 'https://k9b202.p.ssafy.io/api/file/download/893e3c5bcc984f4d9e7bc11549dc3954.png', '열정먼지');
 /*!40000 ALTER TABLE `profile_character` ENABLE KEYS */;
-UNLOCK TABLES;
+UNLOCK
+TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
