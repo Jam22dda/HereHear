a8adeba27302afad72e042a7787d38b487a6cfae
ChanHwan <song960705@naver.com>
2023-11-11 03:23:55 +0900

    feat: validation 예외 발생시 처리하는 exception handler 구현

--- backend/HereHear/src/main/java/com/ssafy/herehear/global/exception/ControllerAdvisor.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/global/exception/ControllerAdvisor.java
@@ -3,6 +3,8 @@ package com.ssafy.herehear.global.exception;
 import com.ssafy.herehear.global.response.CommonResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,12 +15,27 @@ public class ControllerAdvisor {
 
     @ExceptionHandler(CustomException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public CommonResponse customExceptionHandler(CustomException e){
+    public CommonResponse customExceptionHandler(CustomException e) {
         CommonResponse response = new CommonResponse();
         response.setCode(e.getExceptionStatus().getCode());
         response.setMessage(e.getExceptionStatus().getMessage());
         e.printStackTrace();
         return response;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public CommonResponse methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        e.printStackTrace();
+
+        StringBuilder errorMessageBuilder = new StringBuilder();
+        e.getBindingResult().getAllErrors().forEach(error -> {
+            String errorMessage = error.getDefaultMessage();
+            errorMessageBuilder.append(errorMessage).append(", ");
+        });
+        if (errorMessageBuilder.length() > 0) errorMessageBuilder.setLength(errorMessageBuilder.length() - 2);
+
+        return new CommonResponse("400", errorMessageBuilder.toString());
     }
 
     @ExceptionHandler(Exception.class)
