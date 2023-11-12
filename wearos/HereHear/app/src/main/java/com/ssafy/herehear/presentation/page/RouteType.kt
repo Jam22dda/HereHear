0e9c32340e5aa74fee15cfb9343addda0833816a
ChanHwan <song960705@naver.com>
2023-11-12 23:40:12 +0900

    feat: 마이페이지 관련 라우팅 타입 추가

--- wearos/HereHear/app/src/main/java/com/ssafy/herehear/presentation/page/RouteType.kt
+++ wearos/HereHear/app/src/main/java/com/ssafy/herehear/presentation/page/RouteType.kt
@@ -1,5 +1,6 @@
 package com.ssafy.herehear.presentation.page
 
 enum class RouteType {
-    LOADING, LANDING, MAP, MUSIC_LIST, NO_MUSIC, MY_PAGE;
+    LOADING, LANDING, MAP, MUSIC_LIST, NO_MUSIC,
+    MY_PAGE, FOLLOWING, FOLLOWER, RECENT_MUSIC, LIKE_MUSIC, ACHIEVEMENT;
 }
