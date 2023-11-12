90589ac506715a1a888e80810663eb95f22c3de9
samsung <gkathaud@naver.com>
2023-11-12 09:31:43 +0900

    feat: 로그 메시지 타이틀 정의 GlobalConstants 생성

---
+++ backend/HereHear/src/main/java/com/ssafy/herehear/global/util/ConstantsUtil.java
@@ -0,0 +1,14 @@
+package com.ssafy.herehear.global.util;
+
+public final class ConstantsUtil {
+    private ConstantsUtil() {
+        throw new AssertionError("This class should not be instantiated");
+    }
+
+    /**
+     * TotalStats 로그 메시지 타이틀
+     */
+    public static final String TOTAL_STATS_LIKES = "일주일 동안 받은 좋아요 top 4 음악 조회";
+
+
+}
