960501593edf8233d41d12eef09246d83f78c914
cksghks89 <song960705@naver.com>
2023-11-12 16:14:01 +0900

    modify: 인터페이스 이름 변경

--- wearos/HereHear/app/src/main/java/com/ssafy/herehear/presentation/retrofit/service/AuthService.kt
+++ wearos/HereHear/app/src/main/java/com/ssafy/herehear/presentation/retrofit/service/AuthService.kt
@@ -5,7 +5,7 @@ import retrofit2.Call
 import retrofit2.http.POST
 import retrofit2.http.Path
 
-interface PersonalCodeService {
+interface AuthService {
 
     @POST("wearos/auth/{personalCode}")
     fun authPersonalCode(@Path("personalCode") personalCode: String): Call<AuthResponse>
