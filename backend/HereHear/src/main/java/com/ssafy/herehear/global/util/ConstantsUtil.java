c711841f2419013c6e8f0c60cbd5ab4fe0159239
함소명 <gkathaud@naver.com>
2023-11-12 11:37:52 +0900

    feat: TotalStats 로그 메시지 타이틀 추가

--- backend/HereHear/src/main/java/com/ssafy/herehear/global/util/ConstantsUtil.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/global/util/ConstantsUtil.java
@@ -9,6 +9,8 @@ public final class ConstantsUtil {
      * TotalStats 로그 메시지 타이틀
      */
     public static final String TOTAL_STATS_LIKES = "일주일 동안 받은 좋아요 top 4 음악 조회";
+    public static final String TOTAL_STATS_TAGS = "일주일 동안 태그된 단어 top 5 조회";
+    public static final String TOTAL_STATS_HISTORY_MUSIC = "가장 많이 재생된 음악 조회";
 
 
 }
