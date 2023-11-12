fd2f2850c5615200254b92943746f350173962f1
samsung <gkathaud@naver.com>
2023-11-12 10:43:26 +0900

    feat: 전체 통계 태그 조회 Service 추가

--- backend/HereHear/src/main/java/com/ssafy/herehear/totalstats/service/TotalStatsServiceImpl.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/totalstats/service/TotalStatsServiceImpl.java
@@ -3,6 +3,7 @@ package com.ssafy.herehear.totalstats.service;
 import com.ssafy.herehear.entity.RegisteredMusic;
 import com.ssafy.herehear.global.util.ConstantsUtil;
 import com.ssafy.herehear.totalstats.dto.TotalStatsLikesResDto;
+import com.ssafy.herehear.totalstats.dto.TotalStatsTagsResDto;
 import com.ssafy.herehear.totalstats.mapper.TotalStatsMapper;
 import com.ssafy.herehear.totalstats.repository.TotalStatsRepository;
 import jakarta.transaction.Transactional;
@@ -22,7 +23,7 @@ public class TotalStatsServiceImpl implements TotalStatsService {
 
     @Override
     @Transactional
-    public List<TotalStatsLikesResDto> getLikesTop4() {
+    public List<TotalStatsLikesResDto> getLikeCountTop4() {
         log.info("[{}]", ConstantsUtil.TOTAL_STATS_LIKES);
 
         List<TotalStatsLikesResDto> totalStatsLikesResDtos = totalStatsRepository.findByLikesSort().stream()
@@ -32,4 +33,19 @@ public class TotalStatsServiceImpl implements TotalStatsService {
 
         return totalStatsLikesResDtos;
     }
+
+    @Override
+    @Transactional
+    public List<TotalStatsTagsResDto> getTagsTop5() {
+        log.info("[{}]", ConstantsUtil.TOTAL_STATS_TAGS);
+
+        List<TotalStatsTagsResDto> totalStatsLikesResDtos = totalStatsRepository.findByTagsSort().stream()
+                .map(tuple -> totalStatsMapper.toTotalStatsTagsResDto(tuple.get(0, String.class), tuple.get(1, Long.class)))
+                .toList();
+        log.info("[{}] 성공: {}", ConstantsUtil.TOTAL_STATS_TAGS, totalStatsLikesResDtos);
+
+        return totalStatsLikesResDtos;
+    }
+
+
 }
