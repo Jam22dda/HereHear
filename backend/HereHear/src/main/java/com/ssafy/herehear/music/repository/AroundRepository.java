82e1ebe06ad49fb2c25e9034489397703ae29c4e
samsung <gkathaud@naver.com>
2023-11-12 21:27:59 +0900

    modify: around 로그 타이틀 추가

--- backend/HereHear/src/main/java/com/ssafy/herehear/music/repository/AroundRepository.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/music/repository/AroundRepository.java
@@ -6,10 +6,6 @@ import java.util.List;
 
 public interface AroundRepository {
 
-    List<RegisteredMusic> findByRegisterMusics();
-
-    List<String> findByOccasionName(long registeredMusicId);
-
     List<RegisteredMusic> findByAroundSearchMusics(String keyword, List<Long> occasions);
 
 }
