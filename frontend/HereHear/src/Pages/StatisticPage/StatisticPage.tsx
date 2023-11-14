ca580447872109117fdbf866858f5e46c8035ba5
김서영 <qkdxks0922@gmail.com>
2023-11-14 15:57:50 +0900

    fix: 전체 통계 페이지 css 수정

--- frontend/HereHear/src/Pages/StatisticPage/StatisticPage.tsx
+++ frontend/HereHear/src/Pages/StatisticPage/StatisticPage.tsx
@@ -12,7 +12,7 @@ import Heart from "../../assets/CircleButton/icon-heart.png";
 import { useGetLikeStatistics } from "../../apis/Statistics/Quries/useGetLikeStatistics";
 import { useGetTagStatistics } from "../../apis/Statistics/Quries/useGetTagStatistics";
 import { useGetListenStatistics } from "../../apis/Statistics/Quries/useGetListentatistics";
-import { useState } from "react";
+import { useMusicHistory } from "../../apis/Music/Mutations/useMusicHistory";
 
 import { Bar, Pie } from "react-chartjs-2";
 import ChartDataLabels from "chartjs-plugin-datalabels";
@@ -221,6 +221,20 @@ export default function StatisticsPage() {
         ],
     };
 
+    const { mutate: postMusicHostiryMutate } = useMusicHistory();
+
+    const registLikeMusicHistory = () => {
+        if (LikeStatistics[0].registeredMusicId !== null) {
+            postMusicHostiryMutate(LikeStatistics[0].registeredMusicId);
+        }
+    };
+
+    const registListenedMusicHistory = () => {
+        if (ListenStatistics.registeredMusicId !== null) {
+            postMusicHostiryMutate(ListenStatistics.registeredMusicId);
+        }
+    };
+
     return (
         <div id="display">
             <div className="container">
@@ -281,38 +295,52 @@ export default function StatisticsPage() {
                             </S.Label>
                         ))}
                 </S.LabelWrapper>
-                <S.LikeMusicBox>
-                    <MusicItem
-                        src={LoveMyself}
-                        songtitle={musicLabels[0]}
-                        artist={artistLabels[0]}
-                    ></MusicItem>
-                    <CircleButton option="gradActivated" size="large">
-                        <S.HeartContainer>
-                            <Image
-                                src={Heart}
-                                width={25}
-                                $unit="px"
-                                style={{ position: "relative" }}
-                            />
-                            <S.AnimatedHeart
-                                src={Heart}
-                                alt="Heart"
-                                delay={0}
-                            />
-                            <S.AnimatedHeart
-                                src={Heart}
-                                alt="Heart"
-                                delay={0.2}
-                            />{" "}
-                            <S.AnimatedHeart
-                                src={Heart}
-                                alt="Heart"
-                                delay={0.4}
-                            />{" "}
-                        </S.HeartContainer>
-                    </CircleButton>
-                </S.LikeMusicBox>
+                <S.BoxWrapper>
+                    <S.LikeMusicBox
+                        onClick={() => {
+                            registLikeMusicHistory();
+                            const subjectEncoded = encodeURIComponent(
+                                LikeStatistics && LikeStatistics[0].subject
+                            );
+                            const singerEncoded = encodeURIComponent(
+                                LikeStatistics && LikeStatistics[0].singer
+                            );
+                            const youtubeSearchUrl = `https://www.youtube.com/results?search_query=${subjectEncoded}+${singerEncoded}`;
+                            window.location.href = youtubeSearchUrl;
+                        }}
+                    >
+                        <MusicItem
+                            src={LikeStatistics && LikeStatistics[0].albumImg}
+                            songtitle={musicLabels[0]}
+                            artist={artistLabels[0]}
+                        ></MusicItem>
+                        <CircleButton option="gradActivated" size="large">
+                            <S.HeartContainer>
+                                <Image
+                                    src={Heart}
+                                    width={25}
+                                    $unit="px"
+                                    style={{ position: "relative" }}
+                                />
+                                <S.AnimatedHeart
+                                    src={Heart}
+                                    alt="Heart"
+                                    delay={0}
+                                />
+                                <S.AnimatedHeart
+                                    src={Heart}
+                                    alt="Heart"
+                                    delay={0.2}
+                                />{" "}
+                                <S.AnimatedHeart
+                                    src={Heart}
+                                    alt="Heart"
+                                    delay={0.4}
+                                />{" "}
+                            </S.HeartContainer>
+                        </CircleButton>
+                    </S.LikeMusicBox>
+                </S.BoxWrapper>
                 <S.TextWrapper style={{ margin: "60px 0 10px 0" }}>
                     <Text size="body2" style={{ lineHeight: "36px" }}>
                         많은 사람들이
@@ -322,15 +350,9 @@ export default function StatisticsPage() {
                         때
                     </Text>
                 </S.TextWrapper>
-                <Text size="body2" $margin="0 0 8px 0">
-                    음악과 함께 해요
-                </Text>
-                <Pie
-                    style={{ marginTop: "32px" }}
-                    data={piedata}
-                    options={pieoptions}
-                />
-                <S.LabelWrapper>
+                <Text size="body2">음악과 함께 해요</Text>
+                <Pie data={piedata} options={pieoptions} />
+                <S.LabelWrapper style={{ margin: "30px 0 120px 0" }}>
                     {tagNameLabels &&
                         tagNameLabels.map((music, index) => (
                             <S.Label
@@ -344,7 +366,7 @@ export default function StatisticsPage() {
                             </S.Label>
                         ))}
                 </S.LabelWrapper>
-                <Text size="body2" $margin="60px 0 8px 0">
+                <Text size="body2" $margin="0 0 8px 0">
                     가장 많은 사람들이 들은 노래는
                 </Text>
                 <S.TextWrapper>
@@ -363,38 +385,55 @@ export default function StatisticsPage() {
                         이예요
                     </Text>
                 </S.TextWrapper>
-                <S.LikeMusicBox style={{ margin: "30px 0 60px 0" }}>
-                    <MusicItem
-                        src={ListenStatistics && ListenStatistics.albumImg}
-                        songtitle={ListenStatistics && ListenStatistics.subject}
-                        artist={ListenStatistics && ListenStatistics.singer}
-                    ></MusicItem>
-                    <CircleButton option="gradActivated" size="large">
-                        <S.HeartContainer>
-                            <Image
-                                src={Heart}
-                                width={25}
-                                $unit="px"
-                                style={{ position: "relative" }}
-                            />
-                            <S.AnimatedHeart
-                                src={Heart}
-                                alt="Heart"
-                                delay={0}
-                            />
-                            <S.AnimatedHeart
-                                src={Heart}
-                                alt="Heart"
-                                delay={0.2}
-                            />{" "}
-                            <S.AnimatedHeart
-                                src={Heart}
-                                alt="Heart"
-                                delay={0.4}
-                            />{" "}
-                        </S.HeartContainer>
-                    </CircleButton>
-                </S.LikeMusicBox>
+                <S.BoxWrapper>
+                    <S.LikeMusicBox
+                        style={{ marginTop: "50px" }}
+                        onClick={() => {
+                            registListenedMusicHistory();
+                            const subjectEncoded = encodeURIComponent(
+                                ListenStatistics && ListenStatistics.subject
+                            );
+                            const singerEncoded = encodeURIComponent(
+                                ListenStatistics && ListenStatistics.singer
+                            );
+                            const youtubeSearchUrl = `https://www.youtube.com/results?search_query=${subjectEncoded}+${singerEncoded}`;
+                            window.location.href = youtubeSearchUrl;
+                        }}
+                    >
+                        <MusicItem
+                            src={ListenStatistics && ListenStatistics.albumImg}
+                            songtitle={
+                                ListenStatistics && ListenStatistics.subject
+                            }
+                            artist={ListenStatistics && ListenStatistics.singer}
+                        ></MusicItem>
+                        <CircleButton option="gradActivated" size="large">
+                            <S.HeartContainer>
+                                <Image
+                                    src={Heart}
+                                    width={25}
+                                    $unit="px"
+                                    style={{ position: "relative" }}
+                                />
+                                <S.AnimatedHeart
+                                    src={Heart}
+                                    alt="Heart"
+                                    delay={0}
+                                />
+                                <S.AnimatedHeart
+                                    src={Heart}
+                                    alt="Heart"
+                                    delay={0.2}
+                                />{" "}
+                                <S.AnimatedHeart
+                                    src={Heart}
+                                    alt="Heart"
+                                    delay={0.4}
+                                />{" "}
+                            </S.HeartContainer>
+                        </CircleButton>
+                    </S.LikeMusicBox>
+                </S.BoxWrapper>
             </div>
         </div>
     );
