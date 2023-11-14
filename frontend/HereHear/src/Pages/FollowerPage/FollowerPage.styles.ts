19311145651db31d3b04f8d1d396c9511168fcbd
김서영 <qkdxks0922@gmail.com>
2023-11-14 17:03:17 +0900

    fix: 팔로워페이지 css 수정

--- frontend/HereHear/src/Pages/FollowerPage/FollowerPage.styles.ts
+++ frontend/HereHear/src/Pages/FollowerPage/FollowerPage.styles.ts
@@ -2,12 +2,15 @@ import styled from "styled-components";
 
 export const FollowerWrapper = styled.div`
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
     align-items: center;
+    width: 100%;
+    margin-bottom: 16px;
 `;
 
 export const YourFollowerWrapper = styled.div`
     display: flex;
+    flex-direction: column;
+    justify-content: center;
     align-items: center;
-    margin-left: 16px;
 `;
