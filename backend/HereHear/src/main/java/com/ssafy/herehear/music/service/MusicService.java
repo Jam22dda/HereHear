04c95bbe6cc1682343f89deb0a71e0a10f235d83
김주하 <tkfkd122@naver.com>
2023-11-15 02:46:42 +0900

    feat: spotify play, pause, getToken, ChangeVolume, getDeviceId 서비스 정의

--- backend/HereHear/src/main/java/com/ssafy/herehear/music/service/MusicService.java
+++ backend/HereHear/src/main/java/com/ssafy/herehear/music/service/MusicService.java
@@ -7,5 +7,12 @@ import java.util.List;
 public interface MusicService {
 
     List<MusicInfoResDto> getMusicInfoList(String keyword, Integer limit, Integer offset);
+    String getDeviceId(Long memberId);
+    void startMusic(long memberId, String trackId);
+    void pauseMusic(long memberId);
+    void addMusic(long memberId, String trackId);
+    String getToken(long memberId);
+    void updateMemberAccessToken(long memberId);
+    void changeVolume(long memberId, int volume);
 
 }
