dd97ae2c7c98485b0b083f32be7807afaa9f75c5
seol <seolhyehyeon@gmail.com>
2023-11-15 10:10:04 +0900

    fix:통계페이지, 지도시간표시, 너의페이지, 마이페이지 수정

--- frontend/HereHear/src/Pages/FollowerPage/FollowerPage.tsx
+++ frontend/HereHear/src/Pages/FollowerPage/FollowerPage.tsx
@@ -72,23 +72,10 @@ export default function Follower() {
     return (
         <div id="display">
             <div className="container">
-                <CircleButton
-                    option="default2"
-                    size="medium"
-                    onClick={() => navigate(-1)}
-                >
-                    <Image
-                        src={iconBack}
-                        width={10}
-                        height={18}
-                        $unit="px"
-                    ></Image>
+                <CircleButton option="default2" size="medium" onClick={() => navigate(-1)}>
+                    <Image src={iconBack} width={10} height={18} $unit="px"></Image>
                 </CircleButton>
-                <Text
-                    size="subtitle1"
-                    fontWeight="bold"
-                    $margin="30px 0 50px 0"
-                >
+                <Text size="subtitle1" fontWeight="bold" $margin="30px 0 50px 0">
                     팔로워 목록
                 </Text>
                 {yourId === 0
@@ -101,35 +88,28 @@ export default function Follower() {
                                       setYourId(item.memberId);
                                       handleSetYourId(item.memberId);
                                   }}
-                                  characterImage={
-                                      item.profileCharacter.characterImage
-                                  }
+                                  characterImage={item.profileCharacter.characterImage}
                                   nickname={item.nickname}
-                                  titleName={
-                                      item.achievement?.title?.titleName ??
-                                      "뱃지가 없어용!"
-                                  }
+                                  titleName={item.achievement?.title?.titleName ?? "뱃지가 없어용!"}
                               />
                               {item.isFollowed && (
                                   <Button
                                       option="unfollow"
                                       size="medium"
                                       $width="92px"
-                                      onClick={() =>
-                                          handleUnFollowClick(item.memberId)
-                                      }
+                                      style={{ marginLeft: "auto" }}
+                                      onClick={() => handleUnFollowClick(item.memberId)}
                                   >
                                       팔로잉
                                   </Button>
                               )}
                               {!item.isFollowed && (
                                   <Button
+                                      style={{ marginLeft: "auto" }}
                                       option="follow"
                                       size="medium"
                                       $width="92px"
-                                      onClick={() =>
-                                          handleFollowClick(item.memberId)
-                                      }
+                                      onClick={() => handleFollowClick(item.memberId)}
                                   >
                                       팔로우
                                   </Button>
@@ -141,21 +121,12 @@ export default function Follower() {
                           <S.YourFollowerWrapper key={index}>
                               <Follow
                                   onClick={() => {
-                                      navigatePage(
-                                          item.memberId === myId
-                                              ? "/mypage"
-                                              : `/mypage/${item.memberId}`
-                                      );
+                                      navigatePage(item.memberId === myId ? "/mypage" : `/mypage/${item.memberId}`);
                                       setYourId(item.memberId);
                                   }}
-                                  characterImage={
-                                      item.profileCharacter.characterImage
-                                  }
+                                  characterImage={item.profileCharacter.characterImage}
                                   nickname={item.nickname}
-                                  titleName={
-                                      item.achievement?.title?.titleName ??
-                                      "뱃지가 없어용!"
-                                  }
+                                  titleName={item.achievement?.title?.titleName ?? "뱃지가 없어용!"}
                               />
                           </S.YourFollowerWrapper>
                       ))}
