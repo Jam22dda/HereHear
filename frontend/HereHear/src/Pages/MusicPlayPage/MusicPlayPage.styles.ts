9a97b371628c25639cb44651b38e652ee156b1db
김서영 <qkdxks0922@gmail.com>
2023-11-16 16:22:39 +0900

    feat: css 수정, 유튜브 리스트 추가, 스포티파이 인앱실행 추가

--- frontend/HereHear/src/Pages/MusicPlayPage/MusicPlayPage.styles.ts
+++ frontend/HereHear/src/Pages/MusicPlayPage/MusicPlayPage.styles.ts
@@ -12,3 +12,28 @@ export const SelectTagWrapper = styled.div`
     display: flex;
     margin: 10px 0 40px 0;
 `;
+
+export const PlayBtn = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const ProgressBarWrapper = styled.div`
+    width: 300px;
+    height: 5px;
+    background-color: #ddd;
+    border-radius: 5px;
+    cursor: pointer;
+`;
+
+interface ProgressBarProps {
+    width: number;
+}
+
+export const ProgressBar = styled.div<ProgressBarProps>`
+    height: 100%;
+    background: ${({ theme }) => theme.gradient.gradient2};
+    border-radius: 5px;
+    width: ${(props) => props.width}%;
+`;
--- frontend/HereHear/src/Pages/StatisticPage/StatisticPage.tsx
+++ frontend/HereHear/src/Pages/StatisticPage/StatisticPage.tsx
@@ -30,7 +30,17 @@ import {
     // Plugin,
 } from "chart.js";
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, ArcElement, PieController, ChartDataLabels);
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+    ArcElement,
+    PieController,
+    ChartDataLabels
+);
 
 // const shadowPlugin: Plugin<"bar"> = {
 //     // 'bar' 타입을 명시적으로 제공합니다.
@@ -71,7 +81,7 @@ export default function StatisticsPage() {
     const LikeStatistics = useGetLikeStatistics();
     const TagStatistics = useGetTagStatistics();
     const ListenStatistics = useGetListenStatistics();
-    // console.log(LikeStatistics, "LikeStatistics");
+    console.log(LikeStatistics, "LikeStatistics");
     // console.log(TagStatistics);
     // console.log(ListenStatistics);
 
@@ -79,7 +89,9 @@ export default function StatisticsPage() {
         const now = new Date();
         const month = now.toLocaleString("default", { month: "long" }); // 현재 월을 문자열로 가져옵니다.
         const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
-        const weekNumber = Math.ceil((now.getDate() + startOfMonth.getDay()) / 7); // 현재 주차를 계산합니다.
+        const weekNumber = Math.ceil(
+            (now.getDate() + startOfMonth.getDay()) / 7
+        ); // 현재 주차를 계산합니다.
 
         return { month, week: weekNumber };
     }
@@ -95,9 +107,18 @@ export default function StatisticsPage() {
         subject: string;
     }
 
-    const musicLabels: string[] = LikeStatistics && LikeStatistics.length > 0 ? LikeStatistics.map((record: likemusic) => record.subject) : [];
-    const artistLabels: string[] = LikeStatistics && LikeStatistics.length > 0 ? LikeStatistics.map((record: likemusic) => record.singer) : [];
-    const likeCount: string[] = LikeStatistics && LikeStatistics.length > 0 ? LikeStatistics.map((record: likemusic) => record.likeCount) : [];
+    const musicLabels: string[] =
+        LikeStatistics && LikeStatistics.length > 0
+            ? LikeStatistics.map((record: likemusic) => record.subject)
+            : [];
+    const artistLabels: string[] =
+        LikeStatistics && LikeStatistics.length > 0
+            ? LikeStatistics.map((record: likemusic) => record.singer)
+            : [];
+    const likeCount: string[] =
+        LikeStatistics && LikeStatistics.length > 0
+            ? LikeStatistics.map((record: likemusic) => record.likeCount)
+            : [];
 
     const likeColors = ["#FFC0EC", "#FFF0CB", "#BDDDFD", "#F9D6D5"];
     const tagColors = ["#FFC0EC", "#BDDDFD", "#FFF0CB", "#96ebbc", "#F9D6D5"];
@@ -153,9 +174,15 @@ export default function StatisticsPage() {
         tagName: string;
     }
 
-    const tagNameLabels: string[] = TagStatistics && TagStatistics.length > 0 ? TagStatistics.map((record: musicTag) => record.tagName) : [];
+    const tagNameLabels: string[] =
+        TagStatistics && TagStatistics.length > 0
+            ? TagStatistics.map((record: musicTag) => record.tagName)
+            : [];
 
-    const tagCount: string[] = TagStatistics && TagStatistics.length > 0 ? TagStatistics.map((record: musicTag) => record.tagCount) : [];
+    const tagCount: string[] =
+        TagStatistics && TagStatistics.length > 0
+            ? TagStatistics.map((record: musicTag) => record.tagCount)
+            : [];
 
     const pieoptions = {
         responsive: true,
@@ -182,7 +209,13 @@ export default function StatisticsPage() {
             {
                 label: "TagCount",
                 data: tagCount,
-                backgroundColor: ["#FF65D0", "#85C2FF", "#FFDD84", "#75DFA4", "#FAA09F"],
+                backgroundColor: [
+                    "#FF65D0",
+                    "#85C2FF",
+                    "#FFDD84",
+                    "#75DFA4",
+                    "#FAA09F",
+                ],
                 hoverOffset: 6,
                 // borderWidth: 0,
             },
@@ -206,8 +239,17 @@ export default function StatisticsPage() {
     return (
         <div id="display">
             <div className="container">
-                <CircleButton option="default2" size="medium" onClick={() => navigatePage("/core")}>
-                    <Image src={iconBack} width={10} height={18} $unit="px"></Image>
+                <CircleButton
+                    option="default2"
+                    size="medium"
+                    onClick={() => navigatePage("/core")}
+                >
+                    <Image
+                        src={iconBack}
+                        width={10}
+                        height={18}
+                        $unit="px"
+                    ></Image>
                 </CircleButton>
                 <S.TitleWrapper>
                     <Text size="subtitle1" fontWeight="bold">
@@ -234,14 +276,19 @@ export default function StatisticsPage() {
                         에요
                     </Text>
                 </S.TextWrapper>
-                <Bar style={{ marginTop: "30px" }} data={data} options={options} />
+                <Bar
+                    style={{ marginTop: "30px" }}
+                    data={data}
+                    options={options}
+                />
                 <S.LabelWrapper>
                     {musicLabels &&
                         musicLabels.map((music, index) => (
                             <S.Label
                                 key={index}
                                 style={{
-                                    backgroundColor: likeColors[index % likeColors.length],
+                                    backgroundColor:
+                                        likeColors[index % likeColors.length],
                                 }}
                             >
                                 {music}
@@ -252,18 +299,44 @@ export default function StatisticsPage() {
                     <S.LikeMusicBox
                         onClick={() => {
                             registLikeMusicHistory();
-                            const subjectEncoded = encodeURIComponent(LikeStatistics && LikeStatistics[0].subject);
-                            const singerEncoded = encodeURIComponent(LikeStatistics && LikeStatistics[0].singer);
+                            const subjectEncoded = encodeURIComponent(
+                                LikeStatistics && LikeStatistics[0].subject
+                            );
+                            const singerEncoded = encodeURIComponent(
+                                LikeStatistics && LikeStatistics[0].singer
+                            );
                             const youtubeSearchUrl = `https://www.youtube.com/results?search_query=${subjectEncoded}+${singerEncoded}`;
                             window.location.href = youtubeSearchUrl;
                         }}
                     >
-                        <MusicItem src={LikeStatistics && LikeStatistics[0].albumImg} songtitle={musicLabels[0]} artist={artistLabels[0]}></MusicItem>
+                        <MusicItem
+                            src={LikeStatistics && LikeStatistics[0].albumImg}
+                            songtitle={musicLabels[0]}
+                            artist={artistLabels[0]}
+                        ></MusicItem>
                         <CircleButton option="gradActivated" size="large">
                             <S.HeartContainer>
-                                <Image src={Heart} width={25} $unit="px" style={{ position: "relative" }} />
-                                <S.AnimatedHeart src={Heart} alt="Heart" delay={0} />
-                                <S.AnimatedHeart src={Heart} alt="Heart" delay={0.2} /> <S.AnimatedHeart src={Heart} alt="Heart" delay={0.4} />{" "}
+                                <Image
+                                    src={Heart}
+                                    width={25}
+                                    $unit="px"
+                                    style={{ position: "relative" }}
+                                />
+                                <S.AnimatedHeart
+                                    src={Heart}
+                                    alt="Heart"
+                                    delay={0}
+                                />
+                                <S.AnimatedHeart
+                                    src={Heart}
+                                    alt="Heart"
+                                    delay={0.2}
+                                />{" "}
+                                <S.AnimatedHeart
+                                    src={Heart}
+                                    alt="Heart"
+                                    delay={0.4}
+                                />{" "}
                             </S.HeartContainer>
                         </CircleButton>
                     </S.LikeMusicBox>
@@ -272,7 +345,9 @@ export default function StatisticsPage() {
                     <Text size="body2" style={{ lineHeight: "36px" }}>
                         많은 사람들이
                     </Text>
-                    <S.Tag style={{ backgroundColor: tagColors[0] }}>{tagNameLabels[0]}</S.Tag>
+                    <S.Tag style={{ backgroundColor: tagColors[0] }}>
+                        {tagNameLabels[0]}
+                    </S.Tag>
                     <Text size="body2" style={{ lineHeight: "36px" }}>
                         때
                     </Text>
@@ -281,7 +356,11 @@ export default function StatisticsPage() {
                     음악과 함께 해요
                 </Text>
                 <S.chartWrapper>
-                    <Pie style={{ maxWidth: "270px", maxHeight: "270px" }} data={piedata} options={pieoptions} />
+                    <Pie
+                        style={{ maxWidth: "270px", maxHeight: "270px" }}
+                        data={piedata}
+                        options={pieoptions}
+                    />
                 </S.chartWrapper>
                 <S.LabelWrapper2>
                     {tagNameLabels &&
@@ -289,7 +368,8 @@ export default function StatisticsPage() {
                             <S.Label
                                 key={index}
                                 style={{
-                                    backgroundColor: tagColors[index % tagColors.length],
+                                    backgroundColor:
+                                        tagColors[index % tagColors.length],
                                 }}
                             >
                                 {music}
@@ -321,22 +401,46 @@ export default function StatisticsPage() {
                         style={{ marginTop: "30px" }}
                         onClick={() => {
                             registListenedMusicHistory();
-                            const subjectEncoded = encodeURIComponent(ListenStatistics && ListenStatistics.subject);
-                            const singerEncoded = encodeURIComponent(ListenStatistics && ListenStatistics.singer);
+                            const subjectEncoded = encodeURIComponent(
+                                ListenStatistics && ListenStatistics.subject
+                            );
+                            const singerEncoded = encodeURIComponent(
+                                ListenStatistics && ListenStatistics.singer
+                            );
                             const youtubeSearchUrl = `https://www.youtube.com/results?search_query=${subjectEncoded}+${singerEncoded}`;
                             window.location.href = youtubeSearchUrl;
                         }}
                     >
                         <MusicItem
                             src={ListenStatistics && ListenStatistics.albumImg}
-                            songtitle={ListenStatistics && ListenStatistics.subject}
+                            songtitle={
+                                ListenStatistics && ListenStatistics.subject
+                            }
                             artist={ListenStatistics && ListenStatistics.singer}
                         ></MusicItem>
                         <CircleButton option="gradActivated" size="large">
                             <S.HeartContainer>
-                                <Image src={Heart} width={25} $unit="px" style={{ position: "relative" }} />
-                                <S.AnimatedHeart src={Heart} alt="Heart" delay={0} />
-                                <S.AnimatedHeart src={Heart} alt="Heart" delay={0.2} /> <S.AnimatedHeart src={Heart} alt="Heart" delay={0.4} />{" "}
+                                <Image
+                                    src={Heart}
+                                    width={25}
+                                    $unit="px"
+                                    style={{ position: "relative" }}
+                                />
+                                <S.AnimatedHeart
+                                    src={Heart}
+                                    alt="Heart"
+                                    delay={0}
+                                />
+                                <S.AnimatedHeart
+                                    src={Heart}
+                                    alt="Heart"
+                                    delay={0.2}
+                                />{" "}
+                                <S.AnimatedHeart
+                                    src={Heart}
+                                    alt="Heart"
+                                    delay={0.4}
+                                />{" "}
                             </S.HeartContainer>
                         </CircleButton>
                     </S.LikeMusicBox>
